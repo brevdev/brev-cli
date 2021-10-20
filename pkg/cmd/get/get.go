2515d92a734446c0a48dc5b3c4ad76f3a746523a
naderlikeladder <nader.khalil@gmail.com>
2021-10-20 12:17:54 -0700

    updated get command

--- pkg/cmd/get/get.go
+++ pkg/cmd/get/get.go
@@ -2,20 +2,25 @@
 package get
 
 import (
-	"fmt"
-
+	"github.com/brevdev/brev-cli/pkg/auth"
+	"github.com/brevdev/brev-cli/pkg/brev_api"
 	"github.com/brevdev/brev-cli/pkg/cmdcontext"
-	"github.com/brevdev/brev-cli/pkg/portforward"
 	"github.com/brevdev/brev-cli/pkg/terminal"
 
 	"github.com/spf13/cobra"
 )
 
-var sshLong = "dsfsdfds"
+func getOrgs() []brev_api.Organization {
+	
+	token, _ := auth.GetToken()
+	brevAgent := brev_api.Agent{
+		Key: token,
+	}
 
-var sshExample = "dsfsdfsfd"
+	orgs, _ := brevAgent.GetOrgs()
 
-type SshOptions struct{}
+	return orgs
+}
 
 func NewCmdGet(t *terminal.Terminal) *cobra.Command {
 	// opts := SshOptions{}
@@ -34,58 +39,18 @@ func NewCmdGet(t *terminal.Terminal) *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			
+			orgs := getOrgs()
+			// var orgNames []string
+			for _, v := range orgs {
+				// orgNames = append(orgNames, v.Name)
+				t.Vprint(v.Name)
+
+			}
 			t.Vprint(t.Green("heeyyy"))
 			// open(t, orgName, proj)
 			return nil
 		},
 	}
-
-	// cmd := &cobra.Command{
-	// 	Use:                   "ssh TYPE/NAME IDENTIFIER",
-	// 	DisableFlagsInUseLine: true,
-	// 	Short:                 "Get a resource by its identifer",
-	// 	Long:                  sshLong,
-	// 	Example:               sshExample,
-	// 	// Args: func(cmd *cobra.Command, args []string) error {
-	// 	// 	if len(args) <1{
-	// 	// 		return fmt.Errorf("not enough args")
-	// 	// 	} else {
-	// 	// 		return nil
-	// 	// 	}
-
-	// 	// },
-	// 	Args: cobra.MinimumNArgs(1),
-	// 	// ValidArgsFunction: util.ResourceNameCompletionFunc(f, "pod"),
-	// 	Run: func(cmd *cobra.Command, args []string) {
-	// 		cmdutil.CheckErr(opts.Complete(cmd, args))
-	// 		cmdutil.CheckErr(opts.Validate(cmd, args))
-	// 		cmdutil.CheckErr(opts.RunSSH(cmd, args))
-	// 	},
-	// }
 	return cmd
-}
-
-func (o *SshOptions) Complete(cmd *cobra.Command, args []string) error {
-	// return fmt.Errorf("not implemented")
-	return nil
-}
-
-func (o *SshOptions) Validate(cmd *cobra.Command, args []string) error {
-	// return fmt.Errorf("not implemented")
-	return nil
-}
-
-func (o *SshOptions) RunSSH(cmd *cobra.Command, args []string) error {
-	for _, arg := range args {
-		fmt.Println(arg)
-	}
-
-	portforward.RunPortForwardFromCommand(
-		"workspaceNamespace", // TODO find me with info from args(?)
-		"workspacePodName",   // TODO findme
-		[]string{"22", "2022", "2222"},
-		"keyfileString",
-		"certfileString",
-	)
-	return nil
 }
