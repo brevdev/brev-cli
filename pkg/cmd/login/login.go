c998003e08c26a69872e9f3ea7148a495bb3b6e1
f <f@pop-os.localdomain>
2021-10-20 16:57:48 -0700

    auth logic in auth package instead of login command

--- pkg/cmd/login/login.go
+++ pkg/cmd/login/login.go
@@ -2,13 +2,10 @@
 package login
 
 import (
-	"context"
-	"fmt"
 	"time"
 
 	"github.com/brevdev/brev-cli/pkg/auth"
 
-	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
@@ -58,7 +55,6 @@ type app struct {
 	FirstRuns map[string]bool `json:"first_runs"`
 }
 
-
 // tenant is the cli's concept of an auth0 tenant. The fields are tailor fit
 // specifically for interacting with the management API.
 type tenant struct {
@@ -74,58 +70,6 @@ type tenant struct {
 	ClientSecret string `json:"client_secret"`
 }
 
-
-func (o *SshOptions) RunLogin(cmd *cobra.Command, args []string) (error) {
-	ctx := context.Background()
-
-	token, _ := auth.GetToken()
-	if token != nil {
-		fmt.Println("already logged in, log out with:\n\n\tbrev logout\n\n")
-		return nil
-	}
-
-	// TODO env vars
-	authenticator := auth.Authenticator{
-		Audience:           "https://brevdev.us.auth0.com/api/v2/",
-		ClientID:           "JaqJRLEsdat5w7Tb0WqmTxzIeqwqepmk",
-		DeviceCodeEndpoint: "https://brevdev.us.auth0.com/oauth/device/code",
-		OauthTokenEndpoint: "https://brevdev.us.auth0.com/oauth/token",
-	}
-	state, err := authenticator.Start(ctx)
-	if err != nil {
-		return  fmt.Errorf("Could not start the authentication process: %w.", err)
-	}
-
-	// todo color library
-	// fmt.Printf("Your Device Confirmation code is: %s\n\n", ansi.Bold(state.UserCode))
-	// cli.renderer.Infof("%s to open the browser to log in or %s to quit...", ansi.Green("Press Enter"), ansi.Red("^C"))
-	// fmt.Scanln()
-	fmt.Println("Your Devicce Confirmation Code is %s", state.UserCode)
-
-	err = browser.OpenURL(state.VerificationURI)
-
-	if err != nil {
-		fmt.Println("please open: %s", state.VerificationURI)
-	}
-
-	fmt.Println("waiting for auth to complete")
-	var res auth.Result
-
-	res, err = authenticator.Wait(ctx, state)
-
-	if err != nil {
-		return  fmt.Errorf("login error: %w", err)
-	}
-
-	fmt.Print("\n")
-	fmt.Println("Successfully logged in.")
-	fmt.Println("Tenant: %s\n", res.Domain)
-	creds := &auth.Credentials{
-		AccessToken: res.AccessToken,
-		RefreshToken: res.RefreshToken,
-		ExpiresIn: int(res.ExpiresIn),
-	}
-	// store the refresh token
-	auth.WriteTokenToBrevConfigFile(creds)
-	return nil
+func (o *SshOptions) RunLogin(cmd *cobra.Command, args []string) error {
+	return auth.Login()
 }
