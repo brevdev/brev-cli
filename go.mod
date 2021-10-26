41bf33fb24d78f9670cb14c47adb1d744c02bf8b
Alec Fong <alecfong1@gmail.com>
2021-10-25 17:27:06 -0700

    resolving new portforward with link

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 	github.com/schollz/progressbar/v3 v3.8.3
 	github.com/spf13/cobra v1.2.1
-	gopkg.in/square/go-jose.v2 v2.6.0
+	k8s.io/api v0.22.2
+	k8s.io/apimachinery v0.22.2
 	k8s.io/cli-runtime v0.22.2
+	k8s.io/client-go v0.22.2
 	k8s.io/kubectl v0.22.2
 )
--- pkg/cmd/link/link.go
+++ pkg/cmd/link/link.go
@@ -2,20 +2,22 @@
 package link
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/brevdev/brev-cli/pkg/brev_api"
 	"github.com/brevdev/brev-cli/pkg/portforward"
 	"github.com/brevdev/brev-cli/pkg/terminal"
 
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
 func getWorkspaces() []string {
 	// func getWorkspaces(orgID string) []brev_api.Workspace {
 	activeorg, err := brev_api.GetActiveOrgContext()
-
 	if err != nil {
 		return nil
 		// return err
@@ -37,16 +39,35 @@ var sshLinkLong = "Enable a local ssh tunnel, setup private key auth, and give c
 
 var sshLinkExample = "brev link <ws_name>"
 
-type SSHLinkOptions struct {
-	Namespace string
-	PodName   string
-	Port      string
-	KeyFile   string
-	CertFile  string
-}
+var (
+	testCert = ``
+	testKey  = ``
+)
 
-func NewCmdSSHLink(t *terminal.Terminal) *cobra.Command {
-	opts := SSHLinkOptions{}
+func NewCmdSSH(t *terminal.Terminal) *cobra.Command {
+	host := "https://api.k8s.brevstack.com"
+	k8sCert := []byte(testCert)
+	k8sKeyFile := []byte(testKey)
+
+	streams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	k8sClient, err := kubernetes.NewForConfig(&rest.Config{
+		Host: host,
+		TLSClientConfig: rest.TLSClientConfig{
+			CertData: k8sCert,
+			KeyData:  k8sKeyFile,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	opts := portforward.NewPortForwardOptions(
+		k8sClient,
+		&portforward.DefaultPortForwarder{
+			IOStreams: streams,
+		},
+	)
 
 	cmd := &cobra.Command{
 		Use:                   "link",
@@ -58,36 +79,8 @@ func NewCmdSSHLink(t *terminal.Terminal) *cobra.Command {
 		ValidArgs:             getWorkspaces(),
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdutil.CheckErr(opts.Complete(cmd, args))
-			cmdutil.CheckErr(opts.Validate())
-			cmdutil.CheckErr(opts.RunSSH())
+			cmdutil.CheckErr(opts.RunPortforward())
 		},
 	}
 	return cmd
-}
-
-func (o *SSHLinkOptions) Complete(cmd *cobra.Command, args []string) error {
-	for _, arg := range args {
-		fmt.Println(arg)
-	}
-
-	o.Namespace = ""
-	o.PodName = ""
-	o.Port = "22:22"
-	o.KeyFile = ""
-	o.CertFile = ""
-
-	return nil
-}
-
-func (o *SSHLinkOptions) Validate() error {
-	return nil
-}
-
-func (o *SSHLinkOptions) RunSSH() error {
-	portforward.RunPortForwardFromCommand(
-		o.Namespace,
-		o.PodName,
-		[]string{o.Port},
-	)
-	return nil
 }
