485034809cb80c889746253bd054cb848f3b530d
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-10-27 12:15:54 -0700

    modify install script to get uname for bin

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
+
 mkdir -p /tmp/brev
-wget https://github.com/brevdev/brev-cli/releases/download/v0.1.1/brev-cli_0.1.1_linux_amd64.tar.gz -O /tmp/brev/brev.tar.gz
-tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
-sudo cp /tmp/brev/brev /usr/local/bin
-mkdir -p /tmp/brev
-wget https://github.com/brevdev/brev-cli/releases/download/v0.1.1/brev-cli_0.1.1_linux_amd64.tar.gz -O /tmp/brev/brev.tar.gz
+curl -L https://github.com/brevdev/brev-cli/releases/download/v0.1.1/brev-cli_0.1.1_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
 tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
 sudo cp /tmp/brev/brev /usr/local/bin
