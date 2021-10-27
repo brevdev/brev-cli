6bc6033112274398bf3836dea37b52d9ed48e549
Alec Fong <alecfong1@gmail.com>
2021-10-26 17:50:32 -0700

    cleaning up swallowed errors and adding launches to debugger

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,9 +7,9 @@ var config configs
 // TODO add auth0 stuff here instead of being hardcoded in pkg/auth/auth.go
 // Below vars are exposed to the build-layer (Makefile) so that they be overridden at build time.
 var (
-	Version         = "unknown"
-	BrevAPIEndpoint = `https://ade5dtvtaa.execute-api.us-east-1.amazonaws.com`
-	// BrevAPIEndpoint = `http://localhost:8080`
+	Version = "unknown"
+	// BrevAPIEndpoint = `https://ade5dtvtaa.execute-api.us-east-1.amazonaws.com`
+	BrevAPIEndpoint = `http://localhost:8080`
 )
 
 func Init() {
