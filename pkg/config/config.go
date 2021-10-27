4143e7414d84e69210fb7582a3b26ed6c6151ece
naderlikeladder <nader.khalil@gmail.com>
2021-10-27 13:05:20 -0700

    added prod defaults

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -15,10 +15,19 @@ func NewConfig() *Config {
 }
 
 func (c Config) GetBrevAPIURl() string {
+	url := os.Getenv("BREV_API_URL")
+	if url == "" {
+		return "https://ade5dtvtaa.execute-api.us-east-1.amazonaws.com"
+	}
 	return os.Getenv("BREV_API_URL")
 }
 
 func (c Config) GetVersion() string {
+	version := os.Getenv("VERSION")
+
+	if version == "" {
+		return "unknown"
+	}
 	return os.Getenv("VERSION")
 }
 
