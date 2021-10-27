8b4582eee2bc982aea26274cff230134446c776c
f <f@pop-os.localdomain>
2021-10-27 11:51:35 -0700

    use sudo for ubuntu users and correct location

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-#!/usr/bin/env sh
 mkdir -p /tmp/brev
 wget https://github.com/brevdev/brev-cli/releases/download/v0.1.1/brev-cli_0.1.1_linux_amd64.tar.gz -O /tmp/brev/brev.tar.gz
 tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
