7bc5d96fd4731b6f21373d23db178012d47529c2
naderlikeladder <nader.khalil@gmail.com>
2021-10-26 20:30:07 -0700

    .env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,18 +1,39 @@
 // config
 package config
 
-// TODO add auth0 stuff here instead of being hardcoded in pkg/auth/auth.go
-// Below vars are exposed to the build-layer (Makefile) so that they be overridden at build time.
-var (
-	Version = "unknown"
-	// BrevAPIEndpoint = `https://ade5dtvtaa.execute-api.us-east-1.amazonaws.com`
-	BrevAPIEndpoint = `http://localhost:8080`
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type configs struct{}
+
+var config configs
+
+// TODO add auth0 stuff here instead of being hardcoded in pkg/auth/auth.go
+// Below vars are exposed to the build-layer (Makefile) so that they be overridden at build time.
+// var err Error
+
+// var (
+// 	Version = os.Getenv("VERSION")
+// 	BrevAPIEndpoint = os.Getenv("BREVAPIENDPOINT")
+// )
+
 func GetVersion() string {
-	return Version
+	err := godotenv.Load(".env")
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+	return 	os.Getenv("VERSION")
+
 }
 
 func GetBrevAPIEndpoint() string {
-	return BrevAPIEndpoint
+	err := godotenv.Load(".env")
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+	return 	os.Getenv("BREVAPIENDPOINT")
 }
