4c17e9a81a7b6510b6ba0f141765b2865699451d
naderlikeladder <nader.khalil@gmail.com>
2021-10-28 10:12:00 -0700

    ls doesnt require active orgs set

--- pkg/cmd/ls/ls.go
+++ pkg/cmd/ls/ls.go
@@ -93,7 +93,28 @@ func ls(t *terminal.Terminal, args []string) error {
 	}
 	activeorg, err := brev_api.GetActiveOrgContext()
 	if err != nil {
-		return err
+		// TODO: if no active org, print everything
+		orgs, err := getOrgs()
+		if err != nil {
+			return err
+		}
+		if len(orgs) == 0 {
+			t.Vprint(t.Yellow("You don't have any orgs or workspaces. Create an org to get started!"))
+			return nil
+		}
+		for _, o := range orgs {
+			wss, err := getWorkspaces(o.ID)
+			if err != nil {
+				return err
+			}
+			_,_,err = printWorkspaceTable(t, wss, o)
+			if err != nil {
+				return err
+			}
+		}
+		t.Vprint(t.Yellow("\nYou don't have any active org set. Run 'brev set <orgname>' to set one."))
+
+		return nil;
 	}
 
 	wss, err := getWorkspaces(activeorg.ID)
@@ -101,15 +122,33 @@ func ls(t *terminal.Terminal, args []string) error {
 		return err
 	}
 	
-	me := getMe()
+	joined, _, err := printWorkspaceTable(t, wss, *activeorg)
+	if len(joined) > 0 {
+		t.Vprint(t.Green("\nConnect to one with 'brev link <name/id>\n"))
+	}
+
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func truncateString(s string, delimterCount int) string {
+	if len(s) <= delimterCount {
+		return s
+	} else {
+		return s[:delimterCount]
+	}
+}
+
+func printWorkspaceTable(t *terminal.Terminal, workspaces []brev_api.Workspace, activeorg brev_api.Organization)  ([]brev_api.Workspace, []brev_api.Workspace, error) {
+	me := getMe()
 	
 	var unjoinedWorkspaces []brev_api.Workspace
 	var joinedWorkspaces []brev_api.Workspace
 	
-	for _, v := range wss {
+	for _, v := range workspaces {
 		if v.CreatedByUserID == me.Id {
 			joinedWorkspaces = append(joinedWorkspaces, v)
 		} else {
@@ -124,18 +163,7 @@ func ls(t *terminal.Terminal, args []string) error {
 		for _, v := range joinedWorkspaces {
 			t.Vprint(truncateString(v.Name, DELIMETER)+ strings.Repeat(" ", DELIMETER-len(truncateString(v.Name, DELIMETER)))+ " " +v.ID+ strings.Repeat(" ", 5) +v.DNS)
 		}
-		t.Vprint(t.Green("\nConnect to one with 'brev link <name/id>\n"))
 	}
 
-	return nil
-}
-
-
-func truncateString(s string, delimterCount int) string {
-	if len(s) <= delimterCount {
-
-		return s
-	} else {
-		return s[:delimterCount]
-	}
+	return joinedWorkspaces, unjoinedWorkspaces, nil
 }
