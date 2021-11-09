1d5f3acb80c044f9868603c5fc0d0ccb92cb8e80
alecfong1 <alecfong1@gmail.com>
2021-11-09 23:24:50 +0000

    fixing setup script to add golang correctly

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -44,13 +44,8 @@ set -euo pipefail
 (echo ""; echo "##### Golang v16x #####"; echo "";)
 wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
-echo "" | sudo tee -a ~/.bashrc
-echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a ~/.bashrc
-source ~/.bashrc
-echo "" | sudo tee -a ~/.zshrc
-echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a ~/.zshrc
-source ~/.zshrc
 rm go1.16.7.linux-amd64.tar.gz
+printf "\n%s\n" "export PATH=\$PATH:/usr/local/go/bin" | tee -a ~/.bashrc | tee -a ~/.zshrc
 
 # emacs stuff
 cd /tmp
