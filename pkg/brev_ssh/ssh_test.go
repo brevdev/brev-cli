c50e854d18a89e39c726cebaf20614660ac289e3
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-11-11 06:27:33 +0000

    provide better name for ssh & files test suites

--- pkg/brev_ssh/ssh_test.go
+++ pkg/brev_ssh/ssh_test.go
@@ -70,6 +70,6 @@ func (suite *BrevSSHTestSuite) TestFilterExistingWorkspaceNames() {
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestExampleTestSuite(t *testing.T) {
+func TestSSH(t *testing.T) {
 	suite.Run(t, new(BrevSSHTestSuite))
 }
--- pkg/files/files_test.go
+++ pkg/files/files_test.go
@@ -23,6 +23,6 @@ func (s *filesTestSuite) TestGetUserSSHConfigPath() {
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestExampleTestSuite(t *testing.T) {
+func TestFiles(t *testing.T) {
 	suite.Run(t, new(filesTestSuite))
 }
