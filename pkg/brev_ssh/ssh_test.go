6588083b48b113260b77637878238248c56390e7
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-11-11 04:05:55 +0000

    add a failing test for CheckIfBrevHost aand refactor ssh suite setup

--- pkg/brev_ssh/ssh_test.go
+++ pkg/brev_ssh/ssh_test.go
@@ -15,13 +15,13 @@ import (
 // returns the current testing context
 type BrevSSHTestSuite struct {
 	suite.Suite
-	SSHConfig string
+	SSHConfig ssh_config.Config
 }
 
 // Make sure that VariableThatShouldStartAtFive is set to five
 // before each test
 func (suite *BrevSSHTestSuite) SetupTest() {
-	suite.SSHConfig = `
+	r := strings.NewReader( `
 Host brev
 	 Hostname 0.0.0.0
 	 IdentityFile ~/.brev/brev.pem
@@ -39,20 +39,29 @@ Host brevdev/brev-deploy
 	 IdentityFile ~/.brev/brev.pem
 	 User brev
 	 Port 2224
-`
+`)
+	SSHConfig, err := ssh_config.Decode(r)
+
+	if err != nil {
+		panic(err)
+	}
+	suite.SSHConfig = *SSHConfig
 }
 
 // All methods that begin with "Test" are run as tests within a
 // suite.
 func (suite *BrevSSHTestSuite) TestGetBrevPorts() {
-	cfg, err := ssh_config.Decode(strings.NewReader(suite.SSHConfig))
-	if err != nil {
-		panic(err)
+	ports, err := GetBrevPorts(suite.SSHConfig, []string{"brev", "workspace-images", "brevdev/brev-deploy"})
+	suite.True(ports["2222"])
+	suite.True(ports["2223"])
+	suite.True(ports["2224"])
+	suite.Nil(err)
+}
+
+func (suite *BrevSSHTestSuite) TestCheckIfBrevHost() {
+	for _, host := range suite.SSHConfig.Hosts {
+		suite.True(checkIfBrevHost(*host))
 	}
-	ports, err := GetBrevPorts(*cfg, []string{"brev", "workspace-images", "brevdev/brev-deploy"})
-	suite.Assert().True(ports["2222"])
-	suite.Assert().True(ports["2223"])
-	suite.Assert().True(ports["2224"])
 }
 
 // In order for 'go test' to run this suite, we need to create
