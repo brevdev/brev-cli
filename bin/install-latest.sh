df5be735f244fef535f55afb040f4b62d6e9344c
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-11-12 15:14:45 -0800

    Update install-latest.sh

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 mkdir -p /tmp/brev
 curl -L https://github.com/brevdev/brev-cli/releases/download/v0.2.1/brev-cli_0.2.1_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
 tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
