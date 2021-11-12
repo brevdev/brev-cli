03a155d01b425abf28c3593107544f37a9147162
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-11-12 13:16:31 -0800

    Delete install-brev.sh

--- install-brev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p /tmp/brev
-curl -L https://github.com/brevdev/brev-cli/releases/download/v0.1.2/brev-cli_0.1.2_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
-tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
-sudo cp /tmp/brev/brev /usr/local/bin
