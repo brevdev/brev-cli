b6d0f2b6c41a8ac0ca091b049f548aca3ddfd934
nader <nader@brev.dev>
2021-11-19 05:55:03 +0000

    it's all working

--- pkg/cmd/login/login.go
+++ pkg/cmd/login/login.go
@@ -2,6 +2,8 @@
 package login
 
 import (
+	"fmt"
+
 	"github.com/brevdev/brev-cli/pkg/brevapi"
 	breverrors "github.com/brevdev/brev-cli/pkg/errors"
 	"github.com/spf13/cobra"
@@ -30,7 +32,7 @@ func NewCmdLogin(loginStore LoginStore) *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdutil.CheckErr(opts.Complete(cmd, args))
 			cmdutil.CheckErr(opts.Validate(cmd, args))
-			cmdutil.CheckErr(opts.RunLogin(cmd, args))
+			cmdutil.CheckErr(opts.RunLogin(cmd, args, loginStore))
 		},
 	}
 	return cmd
@@ -46,11 +48,45 @@ func (o *LoginOptions) Validate(_ *cobra.Command, _ []string) error {
 	return nil
 }
 
-func (o *LoginOptions) RunLogin(_ *cobra.Command, _ []string) error {
+func (o *LoginOptions) RunLogin(_ *cobra.Command, _ []string, loginStore LoginStore) error {
 	// func (o *LoginOptions) RunLogin(cmd *cobra.Command, args []string) error {
-	err := brevapi.Login(false)
+	token, err := brevapi.Login(false)
 	if err != nil {
 		return breverrors.WrapAndTrace(err)
+	}
+	err = postLogin(*token, loginStore)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	return nil
+}
+
+/* 
+After logging in:
+	> ensure the user is created /Me or create the user
+	> ensure there's an org, or create the first one "username-hq"
+	> 
+*/
+func postLogin(token string, loginStore LoginStore) error {
+	// TODO: hit GetMe and if fails create user
+	client, err := brevapi.NewCommandClient()
+	if err != nil {
+		fmt.Println("error on client")
+		return err
+	}
+	_, err = client.GetMe()
+	if err != nil {
+		fmt.Println("error on getting ME")
+		// TODO: if the error is not a network call create the account
+		// _, err := client.CreateUser(creds.IDToken)
+		_, err = loginStore.CreateUser(token)
+
+		if err != nil {
+			fmt.Println("error on creating user")
+			return err
+		}
+		fmt.Println("created user")
+
 	}
 	return nil
 }
