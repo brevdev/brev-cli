703c8c3d59e83e642360d6ecd5dba4bf6f0daedd
Alec Fong <alecfong1@gmail.com>
2021-11-30 18:06:56 -0800

    update script to use 0.4.0

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p /tmp/brev
-curl -L https://github.com/brevdev/brev-cli/releases/download/v0.3.3/brev-cli_0.3.3_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
+curl -L https://github.com/brevdev/brev-cli/releases/download/v0.4.0/brev-cli_0.4.0_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
 tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
 sudo cp /tmp/brev/brev /usr/local/bin
