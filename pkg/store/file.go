4ca9d715172d59a96684624efb584d30f893d0af
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-12-09 16:37:29 +0000

    add GetOrCreateFile method to filestore

--- pkg/store/file.go
+++ pkg/store/file.go
@@ -1,6 +1,11 @@
 package store
 
-import "github.com/spf13/afero"
+import (
+	"os"
+
+	breverrors "github.com/brevdev/brev-cli/pkg/errors"
+	"github.com/spf13/afero"
+)
 
 type FileStore struct {
 	BasicStore
@@ -9,4 +14,24 @@ type FileStore struct {
 
 func (b *BasicStore) WithFileSystem(fs afero.Fs) *FileStore {
 	return &FileStore{*b, fs}
+}
+
+func (f FileStore) GetOrCreateFile(filepath string) (afero.File, error) {
+	fileExists, err := afero.Exists(f.fs, filepath)
+	if err != nil {
+		return nil, breverrors.WrapAndTrace(err)
+	}
+	var file afero.File
+	if fileExists {
+		file, err = f.fs.OpenFile(filepath, os.O_RDWR, 0o644)
+		if err != nil {
+			return nil, breverrors.WrapAndTrace(err)
+		}
+	} else {
+		file, err = f.fs.Create(filepath)
+		if err != nil {
+			return nil, breverrors.WrapAndTrace(err)
+		}
+	}
+	return file, nil
 }
