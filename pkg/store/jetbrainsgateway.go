90b90cda238ab3b6cf04dc6ca7c37beaadcb4d25
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-12-09 16:40:07 +0000

    add methods to get jetbrains gateway config

---
+++ pkg/store/jetbrainsgateway.go
@@ -0,0 +1,52 @@
+package store
+
+import (
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	breverrors "github.com/brevdev/brev-cli/pkg/errors"
+)
+
+const (
+	JetbrainsGatewayConfigFileName = "sshConfigs.xml"
+)
+
+func (f FileStore) GetJetBrainsConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", breverrors.WrapAndTrace(err)
+	}
+	var infix string
+	infixSuffix := filepath.Join("JetBrains", "JetBrainsGateway2021.3", "options")
+	switch runtime.GOOS {
+	case "windows":
+		return "", errors.New("windows not supported at this time")
+	case "linux":
+		infix = filepath.Join(".config", infixSuffix)
+	case "darwin":
+		infix = filepath.Join("Library", "Application Support", infixSuffix)
+	}
+	sshConfigPath := filepath.Join(home, infix, JetbrainsGatewayConfigFileName)
+	return sshConfigPath, nil
+}
+
+func (f FileStore) GetJetBrainsConfig() (string, error) {
+	path, err := f.GetJetBrainsConfigPath()
+	if err != nil {
+		return "", breverrors.WrapAndTrace(err)
+	}
+	file, err := f.GetOrCreateFile(path)
+	if err != nil {
+		return "", breverrors.WrapAndTrace(err)
+	}
+	buf := new(strings.Builder)
+	_, err = io.Copy(buf, file)
+	if err != nil {
+		return "", breverrors.WrapAndTrace(err)
+	}
+	return buf.String(), nil
+}
