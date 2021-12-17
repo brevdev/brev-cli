26dea543c6ad4b21c32890ea8977613f73dbd3a1
Nader Khalil <nader.khalil@gmail.com>
2021-12-17 09:44:23 -0800

    Update install-latest.sh

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -4,4 +4,4 @@ LATEST_TAG_ABBREV=$(curl -H "Accept: application/vnd.github.v3.text+json" "https
 mkdir -p /tmp/brev
 curl -L https://github.com/brevdev/brev-cli/releases/download/${LATEST_TAG_FULL}/brev-cli_${LATEST_TAG_ABBREV}_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
 tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
-sudo cp /tmp/brev/brev /usr/local/bin
+sudo cp /tmp/brev/brev /usr/bin
