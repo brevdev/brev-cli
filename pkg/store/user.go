d6dccb4346bae670e891e497494bb2d9a80e26f5
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-12-21 02:48:47 +0000

    add get users method to store

--- pkg/store/user.go
+++ pkg/store/user.go
@@ -77,3 +77,20 @@ func (s AuthHTTPStore) SetWorkspaceConfigRepo(userID string, updatedUser *entity
 
 	return nil
 }
+
+func (s AuthHTTPStore) GetUsers(queryParams map[string]string) ([]entity.User, error) {
+	var result []entity.User
+	res, err := s.authHTTPClient.restyClient.R().
+		SetQueryParams(queryParams).
+		SetHeader("Content-Type", "application/json").
+		SetResult(&result).
+		Get(usersPath)
+	if err != nil {
+		return nil, breverrors.WrapAndTrace(err)
+	}
+	if res.IsError() {
+		return nil, NewHTTPResponseError(res)
+	}
+
+	return result, nil
+}
