46af6c418a0557386891f5b6ba986f8b859a6c8b
Alec Fong <alecfong1@gmail.com>
2022-01-05 10:06:29 -0800

    adding coverage for local id

--- pkg/entity/entity_test.go
+++ pkg/entity/entity_test.go
@@ -10,3 +10,42 @@ func TestGetLocalIdentifier(t *testing.T) {
 	w := Workspace{DNS: "test-rand-org.brev.sh"}
 	assert.Equal(t, WorkspaceLocalID("test-rand"), w.GetLocalIdentifier(nil))
 }
+
+func TestGetLocalIdentifierClean(t *testing.T) {
+	// safest https://www.saveonhosting.com/scripts/index.php?rp=/knowledgebase/52/What-are-the-valid-characters-for-a-domain-name-and-how-long-can-a-domain-name-be.html
+	w := Workspace{DNS: "test-rand-org.brev.sh", Name: "abc/def"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+
+	w = Workspace{DNS: "test-rand-org.brev.sh", Name: "'abc"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+
+	w = Workspace{DNS: "test-rand-org.brev.sh", Name: "\"abc"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+
+	w = Workspace{DNS: "test-rand-org.brev.sh", Name: "\\abc"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+
+	w = Workspace{DNS: "test-rand-org.brev.sh", Name: ".abc"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+
+	w = Workspace{DNS: "test-rand-org.brev.sh", Name: ":abc"}
+	assert.Equal(t, WorkspaceLocalID("abc"), w.GetLocalIdentifier(nil))
+}
+
+func TestGetLocalIdentifierDeterminism(t *testing.T) {
+	w1 := WorkspaceWithMeta{Workspace: Workspace{ID: "1", DNS: "main-abc-org.brev.sh", Name: "main"}}
+	w2 := WorkspaceWithMeta{Workspace: Workspace{ID: "2", DNS: "main-def-org.brev.sh", Name: "main"}}
+	ws := []WorkspaceWithMeta{w1, w2}
+
+	assert.Equal(t, WorkspaceLocalID("main-abc"), w1.GetLocalIdentifier(ws))
+	assert.Equal(t, WorkspaceLocalID("main-def"), w2.GetLocalIdentifier(ws))
+
+	// sometime later -- re-arranged
+	ws = []WorkspaceWithMeta{w2, w1}
+	assert.Equal(t, WorkspaceLocalID("main-abc"), w1.GetLocalIdentifier(ws))
+	assert.Equal(t, WorkspaceLocalID("main-def"), w2.GetLocalIdentifier(ws))
+
+	// sometime later -- w2 deleted
+	ws = []WorkspaceWithMeta{w1}
+	assert.Equal(t, WorkspaceLocalID("main-abc"), w1.GetLocalIdentifier(ws))
+}
