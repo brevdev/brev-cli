11df28a4e8051a622600c920520de94c6c74d5eb
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-28 00:20:15 +0000

    Brew formula update for brev-cli version v0.6.12

--- brev-cli.rb
+++ brev-cli.rb
@@ -5,28 +5,28 @@
 class BrevCli < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  version "0.6.11"
+  version "0.6.12"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.11/brev-cli_0.6.11_darwin_amd64.tar.gz"
-      sha256 "65c33e9e9fdcfb0a199f11ed1a4cc9fc0047f726895444d4511c589230b44f5f"
+      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.12/brev-cli_0.6.12_darwin_amd64.tar.gz"
+      sha256 "13fc3162edaa5ffadc45984cb854afceadb489199e791e0e69e091e6ea1a23e6"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.11/brev-cli_0.6.11_darwin_arm64.tar.gz"
-      sha256 "e8eaf629cfa44ca91f24db6ad60e56c1e16b2e9896656b24bab1f0e39765c2f6"
+      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.12/brev-cli_0.6.12_darwin_arm64.tar.gz"
+      sha256 "04582d86683fd6e184b08a1a84b70465937c13aa5088cf4f0c08951131210209"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.11/brev-cli_0.6.11_linux_amd64.tar.gz"
-      sha256 "0c60f6753b87693dfa25f2001574aa7d977857057819ec961c253583f5c8904b"
+      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.12/brev-cli_0.6.12_linux_amd64.tar.gz"
+      sha256 "59718dcc4490c8456b81cb3f5eaab6f8663866f7dcebfafe418e4aa88ab8309e"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.11/brev-cli_0.6.11_linux_arm64.tar.gz"
-      sha256 "3d6bae0338904ee4670404df905ecf70b5a6bb8ed00b31322fb4df495e8f2bb8"
+      url "https://github.com/brevdev/brev-cli/releases/download/v0.6.12/brev-cli_0.6.12_linux_arm64.tar.gz"
+      sha256 "8f61134bd247c9e15847b895f784aed3bdbe0996107742bd0f543457ee1bf991"
     end
   end
 
