6d75c8e4017122050ed1c5e1432333d5755edc82
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-01-27 17:02:00 -0800

    Update install-latest.sh

--- bin/install-latest.sh
+++ bin/install-latest.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-LATEST_TAG_FULL=$(curl -H "Accept: application/vnd.github.v3.text+json" "https://api.github.com/repos/brevdev/brev-cli/tags?per_page=1" |  grep -o "\"v.*\"" | sed 's/"//g')
-LATEST_TAG_ABBREV=$(curl -H "Accept: application/vnd.github.v3.text+json" "https://api.github.com/repos/brevdev/brev-cli/tags?per_page=1" |  grep -o "\"v.*\"" | sed 's/v//' | sed 's/"//g')
-mkdir -p /tmp/brev
-curl -L https://github.com/brevdev/brev-cli/releases/download/${LATEST_TAG_FULL}/brev-cli_${LATEST_TAG_ABBREV}_$(uname | awk '{print tolower($0)}')_amd64.tar.gz > /tmp/brev/brev.tar.gz
-tar -xzvf /tmp/brev/brev.tar.gz -C /tmp/brev
-sudo mkdir -p /opt/brev/bin
-sudo cp /tmp/brev/brev /opt/brev/bin
-export PATH=/opt/brev/bin:$PATH
-echo PATH=/opt/brev/bin:$PATH >> ~/.zshrc
+curl https://raw.githubusercontent.com/brevdev/brev-cli/main/brev-cli.rb > /tmp/brev-cli.rb && brew install /tmp/brev-cli.rb
+
