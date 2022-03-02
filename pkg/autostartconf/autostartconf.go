1acba4a8796774dbe859355b56a17a367cecad1d
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-03-02 00:01:57 +0000

    remove method from autostartconf that is not needed because including store

--- pkg/autostartconf/autostartconf.go
+++ pkg/autostartconf/autostartconf.go
@@ -1,11 +1,8 @@
 package autostartconf
 
-const (
-	targetBin = "/usr/local/bin/brev"
-)
+const targetBin = "/usr/local/bin/brev"
 
 type DaemonConfigurer interface {
-	WriteString(path, data string) error
 	Install() error
 	UnInstall() error
 }
