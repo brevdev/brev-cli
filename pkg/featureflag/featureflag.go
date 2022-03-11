8d16f13c40291f09df18af5b1a69bf455094f0ee
alecfong1 <alecfong1@gmail.com>
2022-03-11 00:54:08 +0000

    grab feature flag dev form os env

--- pkg/featureflag/featureflag.go
+++ pkg/featureflag/featureflag.go
@@ -1,11 +1,12 @@
 package featureflag
 
 import (
+	"os"
 	"strings"
 
 	"github.com/brevdev/brev-cli/pkg/cmd/version"
 )
 
 func IsDev() bool {
-	return version.Version == "" || strings.HasPrefix(version.Version, "dev")
+	return os.Getenv("BREV_FEATURE_FLAG_DEV") == "dev" || version.Version == "" || strings.HasPrefix(version.Version, "dev")
 }
