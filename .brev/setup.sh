1c907c8746be57989b275a4c7bc8f16835144d11
alecfong1 <alecfong1@gmail.com>
2022-04-15 03:06:18 +0000

    add bump version as tool and add make commands for using

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -37,9 +37,16 @@ wget https://golang.org/dl/go1.18.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
 echo "" | sudo tee -a ~/.bashrc
 echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a ~/.bashrc
-source ~/.bashrc
 echo "" | sudo tee -a ~/.zshrc
 echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a ~/.zshrc
-source ~/.zshrc
 rm go1.18.linux-amd64.tar.gz
 
+
+# bumpversion
+sudo apt update
+sudo apt install python3-pip
+pip install --upgrade bump2version
+echo "" | sudo tee -a ~/.bashrc
+echo "export PATH=\$PATH:\$HOME/.local/bin" | sudo tee -a ~/.bashrc
+echo "" | sudo tee -a ~/.zshrc
+echo "export PATH=\$PATH:\$HOME/.local/bin" | sudo tee -a ~/.zshrc
--- Makefile
+++ Makefile
@@ -209,3 +209,19 @@ time:
 
 get-latest-tag:
 	git describe --tags --abbrev=0
+
+version-bump-patch:
+	make version-bump type=patch
+
+version-bump-minor:
+	make version-bump type=minor
+
+version-bump-major:
+	make version-bump type=major
+
+fetch-tags:
+	git fetch --all --tags
+
+version-bump: fetch-tags
+	[ "${type}" ] || ( echo "'type' not provided [patch, minor, major]"; exit 1 )
+	bump2version --current-version $(shell git describe --tags --abbrev=0) ${type} --list --tag --serialize v{major}.{minor}.{patch} --tag-name {new_version}  | grep new_version | sed -r s,"^.*=",, | xargs printf "Version Tagged!\n\nRun: git push origin %s\n\n" 
