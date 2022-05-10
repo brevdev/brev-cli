2c86bd1fca32898efa5fcc31d72d30ef2f12cd58
Alec Fong <alecfong1@gmail.com>
2022-05-10 19:02:19 -0400

    remove unecessary print statements and have json marshall indent

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -117,7 +117,6 @@ func (t Auth) GetFreshAccessTokenOrNil() (string, error) {
 	if err != nil {
 		return "", breverrors.WrapAndTrace(err)
 	}
-	fmt.Printf("access token: %s | is valid: %v\n", tokens.AccessToken, isAccessTokenValid)
 	if !isAccessTokenValid && tokens.RefreshToken != "" {
 		tokens, err = t.getNewTokensWithRefreshOrNil(tokens.RefreshToken)
 		if err != nil {
