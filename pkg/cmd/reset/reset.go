bacc2f926869d2695c8f836c28a902295843e68e
Phanor Coll <phanorcoll@gmail.com>
2022-05-11 14:57:31 -0400

    feature: hard reset for empty workspaces
        Added the capability for --hard reset on empty workspaces, the onces created without a git repository.

--- pkg/cmd/reset/reset.go
+++ pkg/cmd/reset/reset.go
@@ -2,9 +2,13 @@ package reset
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/brevdev/brev-cli/pkg/cmd/completions"
+	"github.com/brevdev/brev-cli/pkg/config"
 	"github.com/brevdev/brev-cli/pkg/entity"
+	"github.com/brevdev/brev-cli/pkg/featureflag"
 	"github.com/brevdev/brev-cli/pkg/store"
 	"github.com/brevdev/brev-cli/pkg/terminal"
 
@@ -14,7 +18,11 @@ import (
 
 var (
 	startLong    = "Reset your machine if it's acting up. This deletes the machine and gets you a fresh one."
-	startExample = "brev reset <ws_name>"
+	startExample = `
+  brev reset <ws_name>
+  brev reset <ws_name> --hardreset
+  brev reset <ws_name> -x
+  `
 )
 
 type ResetStore interface {
@@ -24,11 +32,16 @@ type ResetStore interface {
 	GetWorkspaces(organizationID string, options *store.GetWorkspacesOptions) ([]entity.Workspace, error)
 	GetActiveOrganizationOrDefault() (*entity.Organization, error)
 	GetCurrentUser() (*entity.User, error)
+	CreateWorkspace(organizationID string, options *store.CreateWorkspacesOptions) (*entity.Workspace, error)
 	GetWorkspace(id string) (*entity.Workspace, error)
 	GetWorkspaceMetaData(workspaceID string) (*entity.WorkspaceMetaData, error)
+	DeleteWorkspace(workspaceID string) (*entity.Workspace, error)
+	StartWorkspace(workspaceID string) (*entity.Workspace, error)
 }
 
 func NewCmdReset(t *terminal.Terminal, loginResetStore ResetStore, noLoginResetStore ResetStore) *cobra.Command {
+	var hardreset bool
+
 	cmd := &cobra.Command{
 		Annotations:           map[string]string{"workspace": ""},
 		Use:                   "reset",
@@ -39,16 +52,152 @@ func NewCmdReset(t *terminal.Terminal, loginResetStore ResetStore, noLoginResetS
 		Args:                  cobra.ExactArgs(1),
 		ValidArgsFunction:     completions.GetAllWorkspaceNameCompletionHandler(noLoginResetStore, t),
 		Run: func(cmd *cobra.Command, args []string) {
-			err := resetWorkspace(args[0], t, loginResetStore)
-			if err != nil {
-				t.Vprint(t.Red(err.Error()))
+			if hardreset {
+				err := hardResetProcess(args[0], t, loginResetStore)
+				if err != nil {
+					t.Vprint(t.Red(err.Error()))
+				}
+			} else {
+				err := resetWorkspace(args[0], t, loginResetStore)
+				if err != nil {
+					t.Vprint(t.Red(err.Error()))
+				}
+
 			}
 		},
 	}
 
+	cmd.Flags().BoolVarP(&hardreset, "hardreset", "x", false, "deletes the workspace and creates a fresh version")
 	return cmd
 }
 
+// TODO: delete and create a new empty worksapce 🗹
+// TODO: delete and create a new workspace from a git repo
+// hardResetProcess deletes an existing workspace and creates a new one
+func hardResetProcess(workspaceName string, t *terminal.Terminal, resetStore ResetStore) error {
+	t.Vprint(t.Green(" Starting hard reset 🤙 "))
+	workspace, err := getWorkspaceFromNameOrID(workspaceName, resetStore)
+
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	deletedWorkspace, err := resetStore.DeleteWorkspace(workspace.ID)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	if len(workspace.GitRepo) != 0 {
+		fmt.Println(workspace)
+		err := hardResetCreateWorkspaceFromRepo(deletedWorkspace.GitRepo)
+		if err != nil {
+			return breverrors.WrapAndTrace(err)
+		}
+	} else {
+		err := hardResetCreateEmptyWorkspace(t, resetStore, deletedWorkspace.Name)
+		if err != nil {
+			return breverrors.WrapAndTrace(err)
+		}
+	}
+	return nil
+}
+
+// hardResetCreateWorkspaceFromRepo clone a GIT repository, triggeres from the --hardreset flag
+func hardResetCreateWorkspaceFromRepo(repo string) error {
+	fmt.Println("Cloning repo")
+	fmt.Println(repo)
+	if strings.Contains(repo, "https://") || strings.Contains(repo, "git@") {
+		fmt.Println("valid git repository")
+	}
+	return nil
+}
+
+// hardResetCreateEmptyWorkspace creates a new empty worksapce,  triggered from the --hardreset flag
+func hardResetCreateEmptyWorkspace(t *terminal.Terminal, resetStore ResetStore, name string) error {
+	time.Sleep(10 * time.Second)
+	// ensure name
+	if len(name) == 0 {
+		return fmt.Errorf("name field is required for empty workspaces")
+	}
+
+	// ensure org
+	var orgID string
+	activeorg, err := resetStore.GetActiveOrganizationOrDefault()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	if activeorg == nil {
+		return fmt.Errorf("no org exist")
+	}
+	orgID = activeorg.ID
+	clusterID := config.GlobalConfig.GetDefaultClusterID()
+	options := store.NewCreateWorkspacesOptions(clusterID, name)
+
+	user, err := resetStore.GetCurrentUser()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	options = resolveWorkspaceUserOptions(options, user)
+
+	w, err := resetStore.CreateWorkspace(orgID, options)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	t.Vprint("\nWorkspace is starting. " + t.Yellow("This can take up to 2 minutes the first time.\n"))
+
+	err = pollUntil(t, w.ID, "RUNNING", resetStore, true)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	t.Vprint(t.Green("\nYour workspace is ready!"))
+	t.Vprintf(t.Green("\nSSH into your machine:\n\tssh %s\n", w.GetLocalIdentifier(nil)))
+
+	return nil
+}
+
+func pollUntil(t *terminal.Terminal, wsid string, state string, resetStore ResetStore, canSafelyExit bool) error {
+	s := t.NewSpinner()
+	isReady := false
+	if canSafelyExit {
+		t.Vprintf("You can safely ctrl+c to exit\n")
+	}
+	s.Suffix = " hang tight 🤙"
+	s.Start()
+	for !isReady {
+		time.Sleep(5 * time.Second)
+		ws, err := resetStore.GetWorkspace(wsid)
+		if err != nil {
+			return breverrors.WrapAndTrace(err)
+		}
+		s.Suffix = "  workspace is " + strings.ToLower(ws.Status)
+		if ws.Status == state {
+			s.Suffix = "Workspace is ready!"
+			s.Stop()
+			isReady = true
+		}
+	}
+	return nil
+}
+
+func resolveWorkspaceUserOptions(options *store.CreateWorkspacesOptions, user *entity.User) *store.CreateWorkspacesOptions {
+	if options.WorkspaceTemplateID == "" {
+		if featureflag.IsAdmin(user.GlobalUserType) {
+			options.WorkspaceTemplateID = store.DevWorkspaceTemplateID
+		} else {
+			options.WorkspaceTemplateID = store.UserWorkspaceTemplateID
+		}
+	}
+	if options.WorkspaceClassID == "" {
+		if featureflag.IsAdmin(user.GlobalUserType) {
+			options.WorkspaceClassID = store.DevWorkspaceClassID
+		} else {
+			options.WorkspaceClassID = store.UserWorkspaceClassID
+		}
+	}
+	return options
+}
 
 func resetWorkspace(workspaceName string, t *terminal.Terminal, resetStore ResetStore) error {
 	workspace, err := getWorkspaceFromNameOrID(workspaceName, resetStore)
