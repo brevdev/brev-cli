62b8e86c7ec9e44d07be173a41688467c6fde395
alecfong1 <alecfong1@gmail.com>
2022-05-19 22:29:16 +0000

    resolve tests that were failing due to local idenntifier change

--- pkg/entity/entity_test.go
+++ pkg/entity/entity_test.go
@@ -7,32 +7,17 @@ import (
 )
 
 func TestGetLocalIdentifier(t *testing.T) {
-	w := Workspace{DNS: "test-rand-org.brev.sh"}
-	assert.Equal(t, WorkspaceLocalID("test-rand"), w.GetLocalIdentifier())
+	w := Workspace{DNS: "test-rand-5566-org.brev.sh", ID: "123456789", Name: "test-rand"}
+	assert.Equal(t, WorkspaceLocalID("test-rand-6789"), w.GetLocalIdentifier())
 }
 
 func TestGetLocalIdentifierClean(t *testing.T) {
 	// safest https://www.saveonhosting.com/scripts/index.php?rp=/knowledgebase/52/What-are-the-valid-characters-for-a-domain-name-and-how-long-can-a-domain-name-be.html
-	correctID := WorkspaceLocalID("test-rand")
-	w := Workspace{DNS: "test-rand-org.brev.sh", Name: "abc/def"}
+	correctID := WorkspaceLocalID("test-rand-6789")
+	w := Workspace{DNS: "test-rand-6789-org.brev.sh", Name: "test-rand", ID: "123456789"}
 	assert.Equal(t, correctID, w.GetLocalIdentifier())
 
-	w.Name = "'abc"
-	assert.Equal(t, correctID, w.GetLocalIdentifier())
-
-	w.Name = "\"abc"
-	assert.Equal(t, correctID, w.GetLocalIdentifier())
-
-	w.Name = "\\abc"
-	assert.Equal(t, correctID, w.GetLocalIdentifier())
-
-	w.Name = "/abc"
-	assert.Equal(t, correctID, w.GetLocalIdentifier())
-
-	w.Name = ".abc"
-	assert.Equal(t, correctID, w.GetLocalIdentifier())
-
-	w.Name = ":abc"
+	w.Name = ":./\\\"'test rand"
 	assert.Equal(t, correctID, w.GetLocalIdentifier())
 }
 
@@ -62,12 +47,12 @@ func WorkspacesFromWorkspaceWithMeta(wwm []WorkspaceWithMeta) []Workspace {
 }
 
 func TestGetLocalIdentifierDeterminism(t *testing.T) {
-	w1 := WorkspaceWithMeta{Workspace: Workspace{ID: "1", DNS: "main-abc-org.brev.sh", Name: "main", CreatedByUserID: "user"}}
-	w2 := WorkspaceWithMeta{Workspace: Workspace{ID: "2", DNS: "main-def-org.brev.sh", Name: "main", CreatedByUserID: "user"}}
+	w1 := WorkspaceWithMeta{Workspace: Workspace{ID: "123456789", DNS: "main-6789-org.brev.sh", Name: "main", CreatedByUserID: "user"}}
+	w2 := WorkspaceWithMeta{Workspace: Workspace{ID: "212345678", DNS: "main-5678-org.brev.sh", Name: "main", CreatedByUserID: "user"}}
 
 	// same id must be returned across time
-	w1CorrectID := WorkspaceLocalID("main-abc")
-	w2CorrectID := WorkspaceLocalID("main-def")
+	w1CorrectID := WorkspaceLocalID("main-6789")
+	w2CorrectID := WorkspaceLocalID("main-5678")
 
 	assert.Equal(t, w1CorrectID, w1.GetLocalIdentifier())
 	assert.Equal(t, w2CorrectID, w2.GetLocalIdentifier())
@@ -79,7 +64,7 @@ func TestGetLocalIdentifierDeterminism(t *testing.T) {
 	// sometime later -- w2 deleted
 	assert.Equal(t, w1CorrectID, w1.GetLocalIdentifier())
 
-	// sometime later -- user changes name
+	// sometime later -- user changes name // hmm undefined behavior
 	w1.Name = "new name"
-	assert.Equal(t, w1CorrectID, w1.GetLocalIdentifier())
+	assert.NotEqual(t, w1CorrectID, w1.GetLocalIdentifier())
 }
