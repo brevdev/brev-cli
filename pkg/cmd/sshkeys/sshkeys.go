b1f2b634c286e37221edac52ca5756e90ff0a702
alecfong1 <alecfong1@gmail.com>
2022-05-24 20:43:18 +0000

    improve messaging for ssh-key

--- pkg/cmd/sshkeys/sshkeys.go
+++ pkg/cmd/sshkeys/sshkeys.go
@@ -44,9 +44,11 @@ func NewCmdSSHKeys(t *terminal.Terminal, sshKeyStore SSHKeyStore) *cobra.Command
 }
 
 func DisplaySSHKeys(t *terminal.Terminal, publicKey string) {
-	t.Vprintf("\n" + publicKey + "\n")
+	t.Vprintf(publicKey)
 	t.Print("\n")
 	t.Eprintf(t.Yellow("Copy 👆 and add it to your git provider:\n"))
 	t.Eprintf(t.Yellow("\tGithub: https://github.com/settings/keys\n"))
 	t.Eprintf(t.Yellow("\tGitlab: https://gitlab.com/-/profile/keys\n"))
+	t.Eprintf(t.Yellow("Check authentication by starting a new workspace\n"))
+	t.Eprintf(t.Yellow("\tbrev start --empty --name test-ssh && brev delete test-ssh\n"))
 }
