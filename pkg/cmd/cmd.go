140c6d9848b02c05f54485c6e908af84666fb396
nader <nader@brev.dev>
2022-05-29 15:06:36 +0000

    brev org cmd works

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -16,6 +16,7 @@ import (
 	"github.com/brevdev/brev-cli/pkg/cmd/ls"
 	"github.com/brevdev/brev-cli/pkg/cmd/meshd"
 	"github.com/brevdev/brev-cli/pkg/cmd/open"
+	"github.com/brevdev/brev-cli/pkg/cmd/org"
 	"github.com/brevdev/brev-cli/pkg/cmd/portforward"
 	"github.com/brevdev/brev-cli/pkg/cmd/profile"
 	"github.com/brevdev/brev-cli/pkg/cmd/proxy"
@@ -196,6 +197,7 @@ func NewBrevCommand() *cobra.Command { //nolint:funlen // define brev command
 func createCmdTree(cmd *cobra.Command, t *terminal.Terminal, loginCmdStore *store.AuthHTTPStore, noLoginCmdStore *store.AuthHTTPStore, loginAuth *auth.LoginAuth) {
 	cmd.AddCommand(set.NewCmdSet(t, loginCmdStore, noLoginCmdStore))
 	cmd.AddCommand(ls.NewCmdLs(t, loginCmdStore, noLoginCmdStore))
+	cmd.AddCommand(org.NewCmdOrg(t, loginCmdStore, noLoginCmdStore))
 	cmd.AddCommand(invite.NewCmdInvite(t, loginCmdStore, noLoginCmdStore))
 	cmd.AddCommand(portforward.NewCmdPortForward(loginCmdStore, t))
 	cmd.AddCommand(login.NewCmdLogin(t, noLoginCmdStore, loginAuth))
---
+++ pkg/cmd/org/org.go
@@ -0,0 +1,132 @@
+// Package ls lists workspaces in the current org
+package org
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/brevdev/brev-cli/pkg/cmd/completions"
+	"github.com/brevdev/brev-cli/pkg/cmdcontext"
+	"github.com/brevdev/brev-cli/pkg/entity"
+	breverrors "github.com/brevdev/brev-cli/pkg/errors"
+	"github.com/brevdev/brev-cli/pkg/store"
+	"github.com/brevdev/brev-cli/pkg/terminal"
+	"github.com/jedib0t/go-pretty/v6/table"
+
+	"github.com/spf13/cobra"
+)
+
+type OrgCmdStore interface {
+	GetWorkspaces(organizationID string, options *store.GetWorkspacesOptions) ([]entity.Workspace, error)
+	GetActiveOrganizationOrDefault() (*entity.Organization, error)
+	GetCurrentUser() (*entity.User, error)
+	GetUsers(queryParams map[string]string) ([]entity.User, error)
+	GetWorkspace(workspaceID string) (*entity.Workspace, error)
+	GetOrganizations(options *store.GetOrganizationsOptions) ([]entity.Organization, error)
+}
+
+func NewCmdOrg(t *terminal.Terminal, orgcmdStore OrgCmdStore, noorgcmdStore OrgCmdStore) *cobra.Command {
+	var showAll bool
+	var org string
+
+	cmd := &cobra.Command{
+		Annotations: map[string]string{"context": ""},
+		Use:         "org",
+		Short:       "Organization commands",
+		Long:        "Organization commands",
+		Example: `
+  brev org
+  brev org set
+  brev org new
+  brev org ls
+		`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			err := cmdcontext.InvokeParentPersistentPreRun(cmd, args)
+			if err != nil {
+				return breverrors.WrapAndTrace(err)
+			}
+
+			return nil
+		},
+		Args: cobra.NoArgs,
+		// ValidArgs: []string{"new", "ls"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := RunOrgs(t, orgcmdStore)
+			if err != nil {
+				return breverrors.WrapAndTrace(err)
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&org, "org", "o", "", "organization (will override active org)")
+	err := cmd.RegisterFlagCompletionFunc("org", completions.GetOrgsNameCompletionHandler(noorgcmdStore, t))
+	if err != nil {
+		t.Errprint(err, "cli err")
+	}
+
+	cmd.Flags().BoolVar(&showAll, "all", false, "show all workspaces in org")
+
+	return cmd
+}
+
+func RunOrgs(t *terminal.Terminal, store OrgCmdStore) error {
+	orgs, err := store.GetOrganizations(nil)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	if len(orgs) == 0 {
+		t.Vprint(t.Yellow("You don't have any orgs. Create one! https://console.brev.dev"))
+		return nil
+	}
+
+	defaultOrg, err := store.GetActiveOrganizationOrDefault()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	t.Vprint(t.Yellow("Your organizations:"))
+	displayOrgTable(t, orgs, defaultOrg)
+	if len(orgs) > 1 {
+		fmt.Print("\n")
+		t.Vprintf(t.Green("Switch orgs:\n"))
+		notDefaultOrg := getOtherOrg(orgs, *defaultOrg)
+		// TODO suggest org with max workspaces
+		t.Vprintf(t.Yellow("\tbrev set <NAME> ex: brev set %s\n", notDefaultOrg.Name))
+	}
+
+	return nil
+}
+
+func getBrevTableOptions() table.Options {
+	options := table.OptionsDefault
+	options.DrawBorder = false
+	options.SeparateColumns = false
+	options.SeparateRows = false
+	options.SeparateHeader = false
+	return options
+}
+
+func getOtherOrg(orgs []entity.Organization, org entity.Organization) *entity.Organization {
+	for _, o := range orgs {
+		if org.ID != o.ID {
+			return &o
+		}
+	}
+	return nil
+}
+
+func displayOrgTable(t *terminal.Terminal, orgs []entity.Organization, currentOrg *entity.Organization) {
+	ta := table.NewWriter()
+	ta.SetOutputMirror(os.Stdout)
+	ta.Style().Options = getBrevTableOptions()
+	header := table.Row{"NAME", "ID"}
+	ta.AppendHeader(header)
+	for _, o := range orgs {
+		workspaceRow := []table.Row{{o.Name, o.ID}}
+		if o.ID == currentOrg.ID {
+			workspaceRow = []table.Row{{t.Green("* " + o.Name), t.Green(o.ID)}}
+		}
+		ta.AppendRows(workspaceRow)
+	}
+	ta.Render()
+}
