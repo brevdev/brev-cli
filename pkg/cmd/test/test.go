42a995d85ed54fd5851a92c3e645782bc6b05a34
Nader Khalil <naderkhalil@Naders-MacBook-Pro.local>
2022-05-31 20:24:42 -0400

    reading package.json files

--- pkg/cmd/test/test.go
+++ pkg/cmd/test/test.go
@@ -2,6 +2,10 @@ package test
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/brevdev/brev-cli/pkg/autostartconf"
 	"github.com/brevdev/brev-cli/pkg/cmd/completions"
@@ -9,6 +13,7 @@ import (
 	"github.com/brevdev/brev-cli/pkg/server"
 	"github.com/brevdev/brev-cli/pkg/store"
 	"github.com/brevdev/brev-cli/pkg/terminal"
+	"golang.org/x/text/encoding/charmap"
 
 	breverrors "github.com/brevdev/brev-cli/pkg/errors"
 
@@ -39,7 +44,7 @@ type ServiceMeshStore interface {
 	GetWorkspace(workspaceID string) (*entity.Workspace, error)
 }
 
-func NewCmdTest(_ *terminal.Terminal, _ TestStore) *cobra.Command {
+func NewCmdTest(t *terminal.Terminal, _ TestStore) *cobra.Command {
 	cmd := &cobra.Command{
 		Annotations:           map[string]string{"devonly": ""},
 		Use:                   "test",
@@ -49,13 +54,100 @@ func NewCmdTest(_ *terminal.Terminal, _ TestStore) *cobra.Command {
 		Example:               startExample,
 		// Args:                  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return breverrors.WrapAndTrace(ayo())
+			return breverrors.WrapAndTrace(ayo(t))
 		},
 	}
 
 	return cmd
 }
 
-func ayo() error {
-	return breverrors.WrapAndTrace(fmt.Errorf("test again"))
+// export interface VSCodeExtensionMetadata {
+// 	name: string;
+// 	displayName: string;
+// 	version: string;
+// 	publisher: string;
+// 	description: string;
+// 	repository: RepoMetadata;
+// }
+
+func ayo(t *terminal.Terminal) error {
+	// TODO: read from $HOME/.vscode/extensions
+	paths := recursivelyFindFile(t, []string{"package.json"}, "/Users/naderkhalil/.vscode/extensions")
+	for _, v := range paths {
+		withoutHome := strings.Split(v, "/Users/naderkhalil/")[1]
+
+		// of the format
+		//       .vscode / extensions / extension_name / package.json
+		//          1          2              3               4s
+		if len(strings.Split(withoutHome, "/")) == 4 {
+			t.Vprint(t.Green(withoutHome))
+		}
+
+	}
+	return nil
+}
+
+func catFile(filePath string) (string, error) {
+	gocmd := exec.Command("cat", filePath) // #nosec G204
+	in, err := gocmd.Output()
+	if err != nil {
+		return "", breverrors.WrapAndTrace(err, "error reading file "+filePath)
+	} else {
+		d := charmap.CodePage850.NewDecoder()
+		out, err := d.Bytes(in)
+		if err != nil {
+			return "", breverrors.WrapAndTrace(err, "error reading file "+filePath)
+		}
+		return string(out), nil
+	}
+}
+
+func appendPath(a string, b string) string {
+	if a == "." {
+		return b
+	}
+	return a + "/" + b
+}
+
+// Returns list of paths to file
+func recursivelyFindFile(t *terminal.Terminal, filenames []string, path string) []string {
+	var paths []string
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, f := range files {
+		dir, err := os.Stat(appendPath(path, f.Name()))
+		if err != nil {
+			// fmt.Println(t.Red(err.Error()))
+		} else {
+			for _, filename := range filenames {
+				// r, _ := regexp.Compile(filename)
+				// res := r.MatchString(f.Name())
+
+				if filename == f.Name() {
+					// t.Vprint(t.Yellow(filename) + "---" + t.Yellow(path+f.Name()))
+					paths = append(paths, appendPath(path, f.Name()))
+
+					// fileContents, err := catFile(appendPath(path, f.Name()))
+					// if err != nil {
+					// 	//
+					// }
+
+					// TODO: read
+					// if file has json, read the json
+				}
+			}
+
+			if dir.IsDir() {
+				paths = append(paths, recursivelyFindFile(t, filenames, appendPath(path, f.Name()))...)
+			}
+		}
+	}
+
+	// TODO: make the list Unique
+
+	return paths
 }
