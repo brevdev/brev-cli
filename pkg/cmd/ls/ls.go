109ca0ff26385126facb69fea12e9471b0c28652
Ali Ahmed <ali@Alis-MacBook-Pro-2.local>
2022-06-17 16:46:29 -0700

    Adjusted wording to reset breadcrumb on ls

--- pkg/cmd/ls/ls.go
+++ pkg/cmd/ls/ls.go
@@ -316,7 +316,7 @@ func displayLsResetBreadCrumb(t *terminal.Terminal, workspaces []entity.Workspac
 		}
 	}
 	if foundAResettableWorkspace {
-		t.Vprintf(t.Yellow(fmt.Sprintf("If this problem persists, run the command again with the --hard flag\n\n")))
+		t.Vprintf(t.Yellow(fmt.Sprintf("If this problem persists, run the command again with the --hard flag (warning: the --hard flag will not preserve uncommitted files!) \n\n")))
 	}
 }
 
