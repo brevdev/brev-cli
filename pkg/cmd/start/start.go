d10720a98cf5695834153836b67a2aa02d9c1677
r33drichards <xxmrmau5@gmail.com>
2022-07-11 23:27:56 +0000

    remove extra print statement

--- pkg/cmd/start/start.go
+++ pkg/cmd/start/start.go
@@ -446,7 +446,6 @@ func IsUrl(str string) bool {
 }
 
 func createNewWorkspaceFromGit(user *entity.User, t *terminal.Terminal, setupScriptURLOrPath string, startOptions StartOptions, startStore StartStore) error {
-	t.Vprintf("This is the setup script: %s\n", setupScriptURLOrPath)
 	// https://gist.githubusercontent.com/naderkhalil/4a45d4d293dc3a9eb330adcd5440e148/raw/3ab4889803080c3be94a7d141c7f53e286e81592/setup.sh
 	// fetch contents of file
 	// todo: read contents of file
