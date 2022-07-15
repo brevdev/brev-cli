46c69e2189e24903bb51e600e8c35e4669bd6e23
r33drichards <xxmrmau5@gmail.com>
2022-07-15 23:49:28 +0000

    Revert "add path parameter to GetSetupParams()"
    This reverts commit 4f610ab0a05084ae466df942988928d16ef37d2f.

--- pkg/store/workspace.go
+++ pkg/store/workspace.go
@@ -413,8 +413,8 @@ type (
 	IDEConfigs map[IDEName]IdeConfig
 )
 
-func (f FileStore) GetSetupParams(path string) (*SetupParamsV0, error) {
-	file, err := f.fs.Open(path)
+func (f FileStore) GetSetupParams() (*SetupParamsV0, error) {
+	file, err := f.fs.Open("/etc/meta/setup_v0.json")
 	if err != nil {
 		return nil, breverrors.WrapAndTrace(err)
 	}
