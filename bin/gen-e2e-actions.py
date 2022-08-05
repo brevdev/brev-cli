5e6ac7c02c85e9d520d3137eaa18ea348d63de90
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-08-05 16:41:16 -0700

    only run if commit contains e2etest to save resources

--- bin/gen-e2e-actions.py
+++ bin/gen-e2e-actions.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 USAGE_TEXT = ""
 
+
 def generate_file_content(test_name):
-    return f"name: e2etest-{test_name}\n" + """
+    return (
+        f"name: e2etest-{test_name}\n"
+        + """
 on:
   push:
     branches: [main]
@@ -17,8 +20,11 @@ env:
   BREV_SETUP_TEST_CMD_DIR: /home/brev/workspace/actions-runner/_work/brev-cli/brev-cli
 
 jobs:
-  """ + f"{test_name}:\n" + """
+  """
+        + f"{test_name}:\n"
+        + """
     runs-on: [self-hosted]
+    if: "contains(github.event.head_commit.message, 'e2etest')"
     steps:
       - uses: actions/checkout@v2
       - uses: actions/setup-go@v2
@@ -27,7 +33,9 @@ jobs:
       - name: expire test cache
         run: go clean -testcache
       - name: test
-        run: """ + f"go test -timeout 240s -run ^{test_name}$ github.com/brevdev/brev-cli/e2etest/setup\n"+ """
+        run: """
+        + f"go test -timeout 240s -run ^{test_name}$ github.com/brevdev/brev-cli/e2etest/setup\n"
+        + """
       - name: Report Status
         if: always()
         uses: ravsamhq/notify-slack-action@v1
@@ -37,14 +45,13 @@ jobs:
         env:
           SLACK_WEBHOOK_URL: ${{ secrets.ACTION_MONITORING_SLACK }}
 """
-
+    )
 
 
 def create_file_if_not_exist(path):
     if not os.path.exists(path):
         with open(path, "w") as f:
             f.write("")
-
 
 
 if __name__ == "__main__":
@@ -55,8 +62,7 @@ if __name__ == "__main__":
     file_path_prefix = [".github", "workflows"]
     Path("/".join(file_path_prefix)).mkdir(parents=True, exist_ok=True)
 
-
-    test_names = (name for name in sys.argv[1:] if name != "" )
+    test_names = (name for name in sys.argv[1:] if name != "")
     for test_name in test_names:
         path = Path("/".join(file_path_prefix + [test_name + ".yml"]))
         create_file_if_not_exist(path)
