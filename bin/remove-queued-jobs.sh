65ae45fc5cd84ef0ee9781b44452a9e68837e07d
r33drichards <xxmrmau5@gmail.com>
2022-08-06 00:29:43 +0000

    tidy up

--- bin/remove-queued-jobs.sh
+++ bin/remove-queued-jobs.sh
@@ -1,14 +1,8 @@
 token=$GH_TOKEN
 repo=brevdev/brev-cli
-
-
 # get ids of all queued github actions runs for the repo
-
 ids=$(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$repo/actions/runs?status=queued&per_page=100" | jq -r '.workflow_runs[].id')
-
 set -- $ids
-echo $ids
-
 for i; do curl \
     -H "Authorization: token $token" \
     -X POST "https://api.github.com/repos/$repo/actions/runs/$i/cancel"; done
