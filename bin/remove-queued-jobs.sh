5411f24735c55a30fcfad7e3fb6a82bfcdb9a3ff
r33drichards <xxmrmau5@gmail.com>
2022-08-06 00:27:05 +0000

    add script to cancel queued jobs for cleaning up e2etests

--- bin/remove-queued-jobs.sh
+++ bin/remove-queued-jobs.sh
@@ -1,10 +1,14 @@
 token=$GH_TOKEN
 repo=brevdev/brev-cli
 
-ids=$(curl -s -H "Authorization: token $token" \
-    https://api.github.com/repos/$repo/actions/runs | \
-    jq '.workflow_runs[] | select([.status] | inside(["in_progress", "queued"])) | .id')
+
+# get ids of all queued github actions runs for the repo
+
+ids=$(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$repo/actions/runs?status=queued&per_page=100" | jq -r '.workflow_runs[].id')
+
 set -- $ids
+echo $ids
+
 for i; do curl \
     -H "Authorization: token $token" \
     -X POST "https://api.github.com/repos/$repo/actions/runs/$i/cancel"; done
