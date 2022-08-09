32c5926f6e7494a2647dff21f1caa115e52a8618
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-08-09 16:29:15 -0700

    omit some comments

--- pkg/cmd/hello/hello.go
+++ pkg/cmd/hello/hello.go
@@ -68,14 +68,9 @@ func TypeItToMe(s string) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer ctx.Done()
-	// Exit gracefully by cancelling the ipnserver context in most common cases:
-	// interrupted from the TTY or killed by a service manager.
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
-	// SIGPIPE sometimes gets generated when CLIs disconnect from
-	// tailscaled. The default action is to terminate the process, we
-	// want to keep running.
-	signal.Ignore(syscall.SIGPIPE)
+
 	go func() {
 		for {
 			select {
@@ -83,19 +78,16 @@ func TypeItToMe(s string) {
 				sleepSpeed = sleepSpeed / 2
 
 			case <-interrupt:
-				// fmt.Printf("\r")
 				sleepSpeed = 0
-				// cancel()
+
 			case <-ctx.Done():
 				cancel()
-				// continue
 			}
 		}
 	}()
 
 	sRunes := []rune(s)
 	for i := 0; i < len(sRunes); i++ {
-		// if context cancelled then exit
 		time.Sleep(time.Duration(sleepSpeed) * time.Millisecond)
 		fmt.Printf("%c", sRunes[i])
 	}
