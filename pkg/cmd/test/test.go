df2e8bd2ae3e18edefc05b7e68bbf1875985ac81
r33drichards <xxmrmau5@gmail.com>
2022-08-23 23:31:21 +0000

    unused var

--- pkg/cmd/test/test.go
+++ pkg/cmd/test/test.go
@@ -39,7 +39,7 @@ type ServiceMeshStore interface {
 	GetWorkspace(workspaceID string) (*entity.Workspace, error)
 }
 
-func NewCmdTest(t *terminal.Terminal, _ TestStore) *cobra.Command {
+func NewCmdTest(_ *terminal.Terminal, _ TestStore) *cobra.Command {
 	cmd := &cobra.Command{
 		Annotations:           map[string]string{"devonly": ""},
 		Use:                   "test",
