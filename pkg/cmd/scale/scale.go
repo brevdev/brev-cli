9fe2cb103dd9ee7ee072e37fbc5fbd788b8b6542
nader <nader@brev.dev>
2022-08-25 16:08:01 +0000

    make the ci happy

--- pkg/cmd/scale/scale.go
+++ pkg/cmd/scale/scale.go
@@ -13,11 +13,11 @@ import (
 var (
 	long    = "Scale your Brev environment to get a more powerful machine or save costs"
 	example = `
-  brev start <existing_ws_name>
-  brev start <git url>
-  brev start <git url> --org myFancyOrg
+  brev scale MyDevEnvironment --gpu p3.2xlarge
+  brev scale MyDevEnvironment --cpu 2x8
 	`
-	instanceTypes = []string{"p4d.24xlarge", "p3.2xlarge", "p3.8xlarge", "p3.16xlarge", "p3dn.24xlarge", "p2.xlarge", "p2.8xlarge", "p2.16xlarge", "g5.xlarge", "g5.2xlarge", "g5.4xlarge", "g5.8xlarge", "g5.16xlarge", "g5.12xlarge", "g5.24xlarge", "g5.48xlarge", "g5g.xlarge", "g5g.2xlarge", "g5g.4xlarge", "g5g.8xlarge", "g5g.16xlarge", "g5g.metal", "g4dn.xlarge", "g4dn.2xlarge", "g4dn.4xlarge", "g4dn.8xlarge", "g4dn.16xlarge", "g4dn.12xlarge", "g4dn.metal", "g4ad.xlarge", "g4ad.2xlarge", "g4ad.4xlarge", "g4ad.8xlarge", "g4ad.16xlarge", "g3s.xlarge", "g3.4xlarge", "g3.8xlarge", "g3.16xlarge"}
+	// TODO: we might wanna use the below for validation ¯\_(ツ)_/¯
+	// instanceTypes = []string{"p4d.24xlarge", "p3.2xlarge", "p3.8xlarge", "p3.16xlarge", "p3dn.24xlarge", "p2.xlarge", "p2.8xlarge", "p2.16xlarge", "g5.xlarge", "g5.2xlarge", "g5.4xlarge", "g5.8xlarge", "g5.16xlarge", "g5.12xlarge", "g5.24xlarge", "g5.48xlarge", "g5g.xlarge", "g5g.2xlarge", "g5g.4xlarge", "g5g.8xlarge", "g5g.16xlarge", "g5g.metal", "g4dn.xlarge", "g4dn.2xlarge", "g4dn.4xlarge", "g4dn.8xlarge", "g4dn.16xlarge", "g4dn.12xlarge", "g4dn.metal", "g4ad.xlarge", "g4ad.2xlarge", "g4ad.4xlarge", "g4ad.8xlarge", "g4ad.16xlarge", "g3s.xlarge", "g3.4xlarge", "g3.8xlarge", "g3.16xlarge"}
 )
 
 type ScaleStore interface {
@@ -35,7 +35,7 @@ func NewCmdScale(t *terminal.Terminal, sstore ScaleStore) *cobra.Command {
 		DisableFlagsInUseLine: true,
 		Short:                 "Scale your Brev environment",
 		Long:                  long,
-		Example:               "brev scale MyEnvironment --instance-type p3.2xlarge",
+		Example:               example,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) == 0 {
 				return breverrors.NewValidationError("You must provide an instance with flage -i")
