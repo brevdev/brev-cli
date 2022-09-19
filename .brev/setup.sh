e3ad24ac7b0a0b5c252c779d65128976feae82e9
alecfong1 <alecfong1@gmail.com>
2022-09-19 23:15:39 +0000

    add extensions to setup

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -73,7 +73,18 @@ echo "export PATH=\$PATH:\$HOME/go/bin" | sudo tee -a ~/.bashrc
 echo "" | sudo tee -a ~/.zshrc
 echo "export PATH=\$PATH:\$HOME/go/bin" | sudo tee -a ~/.zshrc
 rm go1.18.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 
+# install golang extension tools
+export GOPATH=$HOME/go
+go install -v golang.org/x/tools/gopls@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v honnef.co/go/tools/cmd/staticcheck@latest
+go install -v github.com/cweill/gotests/gotests@latest
+go install -v github.com/fatih/gomodifytags@latest
+go install -v github.com/josharian/impl@latest
+go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+go install -v github.com/ramya-rao-a/go-outline@latest
 
 # bumpversion
 sudo apt update
