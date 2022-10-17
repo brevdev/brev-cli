892af4d62c1a9eee484fc037cd48217749da566a
r33drichards <xxmrmau5@gmail.com>
2022-10-17 19:15:13 +0000

    dont lint defer statement

--- pkg/store/aws.go
+++ pkg/store/aws.go
@@ -18,7 +18,7 @@ func (n *NoAuthHTTPStore) GetInstanceType() (string, error) {
 		return "", breverrors.WrapAndTrace(err)
 	}
 
-	defer resp.Body.Close()
+	defer resp.Body.Close() //nolint:errcheck // defer
 	err = json.NewDecoder(resp.Body).Decode(&iid)
 	if err != nil {
 		return "", breverrors.WrapAndTrace(err)
