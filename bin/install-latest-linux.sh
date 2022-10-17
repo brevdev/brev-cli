784cfc0f18a0af402f5452abe2641eeaf0fd8631
r33drichards <xxmrmau5@gmail.com>
2022-10-17 20:21:35 +0000

    new isntall script

--- bin/install-latest-linux.sh
+++ bin/install-latest-linux.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
-# Install the latest version of the Linux binary
+#!/usr/bin/env bash
 
 set -eo pipefail
+
+# Install the latest version of the Linux binary
 
 # Get THE DOWNLOAD URL
 DOWNLOAD_URL=$(curl -s https://brevapi.us-west-2-prod.control-plane.brev.dev/api/autostop/cli-download-url)
@@ -9,22 +10,22 @@ DOWNLOAD_URL=$(curl -s https://brevapi.us-west-2-prod.control-plane.brev.dev/api
 # download the tar to a tmp directory
 
 TMP_DIR=$(mktemp -d)
-curl -sL "$DOWNLOAD_URL" -o "$TMP_DIR/brev.tar.gz"
+
+wget --directory-prefix=$TMP_DIR $DOWNLOAD_URL
 
 # extract the tar to the bin directory
-tar -xzf "$TMP_DIR/brev.tar.gz" -C "$TMP_DIR"
+tar -xzf $TMP_DIR/brev* -C $TMP_DIR # glob is a hack to get the filename
 
 # move the binary to the bin directory
-mv "$TMP_DIR/brev" /usr/local/bin/brev
+mv $TMP_DIR/brev /usr/local/bin/brev
 
 # remove the tmp directory
-rm -rf "$TMP_DIR"
+rm -rf $TMP_DIR
 
 # make the binary executable
 chmod +x /usr/local/bin/brev
 
 # run post install commands, write now creates a file in etc
 # to store email so needs root
-
 
 sudo brev postinstall
