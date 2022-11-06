ed9a0f4c86154c8a9a71b6bb490613dc31247f96
r33drichards <xxmrmau5@gmail.com>
2022-11-06 00:46:57 +0000

    add pkg

---
+++ pkg/cmd/bmon/bmon.go
@@ -0,0 +1,50 @@
+package bmon
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/brevdev/brev-cli/pkg/autostartconf"
+	breverrors "github.com/brevdev/brev-cli/pkg/errors"
+	"github.com/brevdev/brev-cli/pkg/terminal"
+)
+
+var (
+	short   = "TODO"
+	long    = "TODO"
+	example = "TODO"
+)
+
+type bmonStore interface {
+	autostartconf.AutoStartStore
+}
+
+func NewCmdbmon(t *terminal.Terminal, store bmonStore) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                   "bmon",
+		DisableFlagsInUseLine: true,
+		Short:                 short,
+		Long:                  long,
+		Example:               example,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := Runbmon(t, args, store)
+			if err != nil {
+				return breverrors.WrapAndTrace(err)
+			}
+			return nil
+		},
+	}
+	return cmd
+}
+
+func Runbmon(_ *terminal.Terminal, _ []string, store bmonStore) error {
+	bmonConfig := autostartconf.NewBrevMonConfigure(
+		store,
+		false,
+		"10m",
+	)
+	err := bmonConfig.Install()
+	if err != nil {
+		return err
+	}
+	return nil
+}
