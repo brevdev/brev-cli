53055f506de9dcb79569f6063c99d166484ac8ec
r33drichards <xxmrmau5@gmail.com>
2022-11-11 19:01:47 +0000

    add test back

--- pkg/ssh/sshconfigurer_test.go
+++ pkg/ssh/sshconfigurer_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 
 	"github.com/brevdev/brev-cli/pkg/entity"
+	"github.com/brevdev/brev-cli/pkg/store"
 	"github.com/google/go-cmp/cmp"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -354,211 +356,211 @@ func Test_makeSSHConfigEntryV2(t *testing.T) { //nolint:funlen // test
 	}
 }
 
-// func makeMockFS() *store.FileStore {
-// 	bs := store.NewBasicStore().WithEnvGetter(
-// 		func(s string) string {
-// 			return "test"
-// 		},
-// 	)
-// 	fs := bs.WithFileSystem(afero.NewMemMapFs())
-// 	return fs
-// }
+func makeMockFS() *store.FileStore {
+	bs := store.NewBasicStore().WithEnvGetter(
+		func(s string) string {
+			return "test"
+		},
+	)
+	fs := bs.WithFileSystem(afero.NewMemMapFs())
+	return fs
+}
 
-// func makeMockWSLFS() *store.FileStore {
-// 	bs := store.NewBasicStore().WithEnvGetter(
-// 		func(s string) string {
-// 			return "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Users/15854/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/15854/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin"
-// 		},
-// 	)
-// 	f := afero.NewMemMapFs()
-// 	fs := bs.WithFileSystem(f)
-// 	dir, err := fs.GetWindowsDir()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	err = f.MkdirAll(dir, 0o755)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return fs
-// }
+func makeMockWSLFS() *store.FileStore {
+	bs := store.NewBasicStore().WithEnvGetter(
+		func(s string) string {
+			return "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Users/15854/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/15854/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin"
+		},
+	)
+	f := afero.NewMemMapFs()
+	fs := bs.WithFileSystem(f)
+	dir, err := fs.GetWindowsDir()
+	if err != nil {
+		panic(err)
+	}
+	err = f.MkdirAll(dir, 0o755)
+	if err != nil {
+		panic(err)
+	}
+	return fs
+}
 
-// func TestSSHConfigurerV2_Update(t *testing.T) { //nolint:funlen // this is a test
-// 	type fields struct {
-// 		store        SSHConfigurerV2Store
-// 		runRemoteCMD bool
-// 	}
-// 	type args struct {
-// 		workspaces []entity.Workspace
-// 	}
-// 	tests := []struct {
-// 		name                   string
-// 		fields                 fields
-// 		args                   args
-// 		wantErr                bool
-// 		linuxSSHConfig         string
-// 		linuxBrevSSHConfig     string
-// 		windowsSSHConfig       string
-// 		windowsBrevSSHConfig   string
-// 		windowsSSHConfigExists bool
-// 		skip                   bool
-// 	}{
-// 		// TODO: Add test cases.
-// 		{
-// 			name: "test update",
-// 			fields: fields{
-// 				store:        makeMockFS(),
-// 				runRemoteCMD: false,
-// 			},
-// 			args: args{
-// 				workspaces: []entity.Workspace{
-// 					{
-// 						ID:               "test-id-1",
-// 						Name:             "testName1",
-// 						WorkspaceGroupID: "test-id-1",
-// 						OrganizationID:   "oi",
-// 						WorkspaceClassID: "wci",
-// 						CreatedByUserID:  "cui",
-// 						DNS:              "test1-dns-org.brev.sh",
-// 						Status:           entity.Running,
-// 						Password:         "sdfal",
-// 						GitRepo:          "gitrepo",
-// 					},
-// 				},
-// 			},
-// 			wantErr:        false,
-// 			linuxSSHConfig: "Include /home/ubuntu/.brev/ssh_config\n",
-// 			linuxBrevSSHConfig: `# included in /home/ubuntu/.ssh/config
-// Host testName1
-//   Hostname test1-dns-org.brev.sh
-//   IdentityFile /home/ubuntu/.brev/brev.pem
-//   User ubuntu
-//   ServerAliveInterval 30
-//   UserKnownHostsFile /dev/null
-//   StrictHostKeyChecking no
-//   PasswordAuthentication no
-//   RequestTTY yes
+func TestSSHConfigurerV2_Update(t *testing.T) { //nolint:funlen // this is a test
+	type fields struct {
+		store        SSHConfigurerV2Store
+		runRemoteCMD bool
+	}
+	type args struct {
+		workspaces []entity.Workspace
+	}
+	tests := []struct {
+		name                   string
+		fields                 fields
+		args                   args
+		wantErr                bool
+		linuxSSHConfig         string
+		linuxBrevSSHConfig     string
+		windowsSSHConfig       string
+		windowsBrevSSHConfig   string
+		windowsSSHConfigExists bool
+		skip                   bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test update",
+			fields: fields{
+				store:        makeMockFS(),
+				runRemoteCMD: false,
+			},
+			args: args{
+				workspaces: []entity.Workspace{
+					{
+						ID:               "test-id-1",
+						Name:             "testName1",
+						WorkspaceGroupID: "test-id-1",
+						OrganizationID:   "oi",
+						WorkspaceClassID: "wci",
+						CreatedByUserID:  "cui",
+						DNS:              "test1-dns-org.brev.sh",
+						Status:           entity.Running,
+						Password:         "sdfal",
+						GitRepo:          "gitrepo",
+					},
+				},
+			},
+			wantErr:        false,
+			linuxSSHConfig: "Include /home/ubuntu/.brev/ssh_config\n",
+			linuxBrevSSHConfig: `# included in /home/ubuntu/.ssh/config
+Host testName1
+  Hostname test1-dns-org.brev.sh
+  IdentityFile /home/ubuntu/.brev/brev.pem
+  User ubuntu
+  ServerAliveInterval 30
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  RequestTTY yes
 
-// `,
+`,
 
-// 			windowsSSHConfig:       ``,
-// 			windowsBrevSSHConfig:   ``,
-// 			windowsSSHConfigExists: false,
-// 		},
-// 		{
-// 			name: "test update with windows",
-// 			fields: fields{
-// 				store:        makeMockWSLFS(),
-// 				runRemoteCMD: false,
-// 			},
-// 			args: args{
-// 				workspaces: []entity.Workspace{
-// 					{
-// 						ID:               "test-id-1",
-// 						Name:             "testName1",
-// 						WorkspaceGroupID: "test-id-1",
-// 						OrganizationID:   "oi",
-// 						WorkspaceClassID: "wci",
-// 						CreatedByUserID:  "cui",
-// 						DNS:              "test1-dns-org.brev.sh",
-// 						Status:           entity.Running,
-// 						Password:         "sdfal",
-// 						GitRepo:          "gitrepo",
-// 					},
-// 				},
-// 			},
-// 			wantErr:        false,
-// 			linuxSSHConfig: "Include /home/ubuntu/.brev/ssh_config\n",
-// 			linuxBrevSSHConfig: `# included in /home/ubuntu/.ssh/config
-// Host testName1
-//   Hostname test1-dns-org.brev.sh
-//   IdentityFile /home/ubuntu/.brev/brev.pem
-//   User ubuntu
-//   ServerAliveInterval 30
-//   UserKnownHostsFile /dev/null
-//   StrictHostKeyChecking no
-//   PasswordAuthentication no
-//   RequestTTY yes
+			windowsSSHConfig:       ``,
+			windowsBrevSSHConfig:   ``,
+			windowsSSHConfigExists: false,
+		},
+		{
+			name: "test update with windows",
+			fields: fields{
+				store:        makeMockWSLFS(),
+				runRemoteCMD: false,
+			},
+			args: args{
+				workspaces: []entity.Workspace{
+					{
+						ID:               "test-id-1",
+						Name:             "testName1",
+						WorkspaceGroupID: "test-id-1",
+						OrganizationID:   "oi",
+						WorkspaceClassID: "wci",
+						CreatedByUserID:  "cui",
+						DNS:              "test1-dns-org.brev.sh",
+						Status:           entity.Running,
+						Password:         "sdfal",
+						GitRepo:          "gitrepo",
+					},
+				},
+			},
+			wantErr:        false,
+			linuxSSHConfig: "Include /home/ubuntu/.brev/ssh_config\n",
+			linuxBrevSSHConfig: `# included in /home/ubuntu/.ssh/config
+Host testName1
+  Hostname test1-dns-org.brev.sh
+  IdentityFile /home/ubuntu/.brev/brev.pem
+  User ubuntu
+  ServerAliveInterval 30
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  RequestTTY yes
 
-// `,
-// 			windowsSSHConfig: `# included in C:\\Users\\15854\\.ssh\\config
-// Host testName1
-//   Hostname test1-dns-org.brev.sh
-//   IdentityFile C:\\Users\\15854\\.brev\\brev.pem
-//   User ubuntu
-//   ServerAliveInterval 30
-//   UserKnownHostsFile /dev/null
-//   StrictHostKeyChecking no
-//   PasswordAuthentication no
-//   RequestTTY yes
+`,
+			windowsSSHConfig: `# included in C:\\Users\\15854\\.ssh\\config
+Host testName1
+  Hostname test1-dns-org.brev.sh
+  IdentityFile C:\\Users\\15854\\.brev\\brev.pem
+  User ubuntu
+  ServerAliveInterval 30
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  RequestTTY yes
 
-// `,
-// 			windowsBrevSSHConfig: `# included in C:\\Users\\15854\\.brev\\ssh_config
-// Host testName1
-//   Hostname test1-dns-org.brev.sh
-//   IdentityFile C:\\Users\\15854\\.brev\\brev.pem
-//   User ubuntu
-//   ServerAliveInterval 30
-//   UserKnownHostsFile /dev/null
-//   StrictHostKeyChecking no
-//   PasswordAuthentication no
-//   RequestTTY yes
+`,
+			windowsBrevSSHConfig: `# included in C:\\Users\\15854\\.brev\\ssh_config
+Host testName1
+  Hostname test1-dns-org.brev.sh
+  IdentityFile C:\\Users\\15854\\.brev\\brev.pem
+  User ubuntu
+  ServerAliveInterval 30
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  RequestTTY yes
 
-// `,
-// 			windowsSSHConfigExists: true,
-// 			skip:                   true,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if tt.skip {
-// 				t.Skip()
-// 			}
-// 			s := SSHConfigurerV2{
-// 				store: tt.fields.store,
-// 			}
-// 			if err := s.Update(tt.args.workspaces); (err != nil) != tt.wantErr {
-// 				t.Errorf("SSHConfigurerV2.Update() error = %v, wantErr %v", err, tt.wantErr)
-// 			}
-// 			// make sure the linux config is correct
-// 			linuxConfig, err := s.store.GetFileAsString("/home/ubuntu/.ssh/config")
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			diff := cmp.Diff(tt.linuxSSHConfig, linuxConfig)
-// 			if diff != "" {
-// 				t.Fatalf(diff)
-// 			}
+`,
+			windowsSSHConfigExists: true,
+			skip:                   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.skip {
+				t.Skip()
+			}
+			s := SSHConfigurerV2{
+				store: tt.fields.store,
+			}
+			if err := s.Update(tt.args.workspaces); (err != nil) != tt.wantErr {
+				t.Errorf("SSHConfigurerV2.Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			// make sure the linux config is correct
+			linuxConfig, err := s.store.GetFileAsString("/home/ubuntu/.ssh/config")
+			if err != nil {
+				t.Fatal(err)
+			}
+			diff := cmp.Diff(tt.linuxSSHConfig, linuxConfig)
+			if diff != "" {
+				t.Fatalf(diff)
+			}
 
-// 			linuxBrevSSHConfig, err := s.store.GetFileAsString("/home/ubuntu/.brev/ssh_config")
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			diff = cmp.Diff(tt.linuxBrevSSHConfig, linuxBrevSSHConfig)
-// 			if diff != "" {
-// 				t.Fatalf(diff)
-// 			}
+			linuxBrevSSHConfig, err := s.store.GetFileAsString("/home/ubuntu/.brev/ssh_config")
+			if err != nil {
+				t.Fatal(err)
+			}
+			diff = cmp.Diff(tt.linuxBrevSSHConfig, linuxBrevSSHConfig)
+			if diff != "" {
+				t.Fatalf(diff)
+			}
 
-// 			// if tt.windowsSSHConfigExists {
-// 			// 	// make sure the windows config is correct
-// 			// 	windowsConfig, err := tt.fields.store.GetFileAsString("/mnt/c/Users/15854/.ssh/config")
-// 			// 	if err != nil {
-// 			// 		t.Fatal(err)
-// 			// 	}
-// 			// 	diff = cmp.Diff(tt.windowsSSHConfig, windowsConfig)
-// 			// 	if diff != "" {
-// 			// 		t.Fatalf(diff)
-// 			// 	}
+			if tt.windowsSSHConfigExists {
+				// make sure the windows config is correct
+				windowsConfig, err := tt.fields.store.GetFileAsString("/mnt/c/Users/15854/.ssh/config")
+				if err != nil {
+					t.Fatal(err)
+				}
+				diff = cmp.Diff(tt.windowsSSHConfig, windowsConfig)
+				if diff != "" {
+					t.Fatalf(diff)
+				}
 
-// 			// 	windowsBrevSSHConfig, err := s.store.GetFileAsString("/mnt/c/Users/15854/.brev/ssh_config")
-// 			// 	if err != nil {
-// 			// 		t.Fatal(err)
-// 			// 	}
-// 			// 	diff = cmp.Diff(tt.windowsBrevSSHConfig, windowsBrevSSHConfig)
-// 			// 	if diff != "" {
-// 			// 		t.Fatalf(diff)
-// 			// 	}
-// 			// }
-// 		})
-// 	}
-// }
+				windowsBrevSSHConfig, err := s.store.GetFileAsString("/mnt/c/Users/15854/.brev/ssh_config")
+				if err != nil {
+					t.Fatal(err)
+				}
+				diff = cmp.Diff(tt.windowsBrevSSHConfig, windowsBrevSSHConfig)
+				if diff != "" {
+				t.Fatalf(diff)
+				}
+			}
+		})
+	}
+}
