36357af8c633acd2c91a87bfcc18a85bbd0dc39f
Nader Khalil <naderkhalil@Naders-MacBook-Pro.local>
2022-11-22 16:08:27 -0800

    update the new onboarding status section

--- pkg/cmd/hello/updateUser.go
+++ pkg/cmd/hello/updateUser.go
@@ -14,6 +14,7 @@ import (
 func SkippedOnboarding(user *entity.User, store HelloStore) error {
 	newOnboardingStatus := make(map[string]interface{})
 	newOnboardingStatus["cliOnboardingSkipped"] = true
+	newOnboardingStatus["finishedOnboarding"] = true
 
 	_, err := store.UpdateUser(user.ID, &entity.UpdateUser{
 		// username, name, and email are required fields, but we only care about onboarding status
