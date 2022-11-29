1142965ed144d1648a92fda2495e94d3e738f54d
r33drichards <xxmrmau5@gmail.com>
2022-11-29 19:12:52 +0000

    add tests for overwrite string and connection event

--- pkg/store/file_test.go
+++ pkg/store/file_test.go
@@ -5,8 +5,10 @@ import (
 	"os"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	user "github.com/tweekmonster/luser"
 )
 
 func TestWithFileSystem(t *testing.T) {
@@ -221,6 +223,113 @@ func TestFileStore_AppendString(t *testing.T) {
 			}
 			if err := f.AppendString(tt.args.path, tt.args.content); (err != nil) != tt.wantErr {
 				t.Errorf("FileStore.AppendString() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFileStore_OverWriteString(t *testing.T) {
+	bs := MakeMockBasicStore()
+	fs := afero.NewMemMapFs()
+	type fields struct {
+		b                 BasicStore
+		fs                afero.Fs
+		User              *user.User
+		userHomeDirGetter func() (string, error)
+	}
+	type args struct {
+		path    string
+		content string
+	}
+	tests := []struct {
+		name                 string
+		fields               fields
+		args                 args
+		wantErr              bool
+		expectedFileContents string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "overwrite file",
+			fields: fields{
+				*bs, fs, nil, nil,
+			},
+			args: args{
+				"foo",
+				"bar",
+			},
+			wantErr:              false,
+			expectedFileContents: "bar",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := FileStore{
+				b:                 tt.fields.b,
+				fs:                tt.fields.fs,
+				User:              tt.fields.User,
+				userHomeDirGetter: tt.fields.userHomeDirGetter,
+			}
+			if err := f.OverWriteString(tt.args.path, tt.args.content); (err != nil) != tt.wantErr {
+				t.Errorf("FileStore.OverWriteString() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			// call twice to make sure it overwrites
+			if err := f.OverWriteString(tt.args.path, tt.args.content); (err != nil) != tt.wantErr {
+				t.Errorf("FileStore.OverWriteString() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			fileContents, err := f.GetFileAsString(tt.args.path)
+			if err != nil {
+				t.Errorf("error reading file %s", err)
+			}
+			difference := cmp.Diff(fileContents, tt.args.content)
+			if difference != "" {
+				t.Errorf("file contents don't match expected %s", difference)
+			}
+		})
+	}
+}
+
+func TestFileStore_WriteConnectionEvent(t *testing.T) {
+	bs := MakeMockBasicStore()
+	fs := afero.NewMemMapFs()
+	type fields struct {
+		b                 BasicStore
+		fs                afero.Fs
+		User              *user.User
+		userHomeDirGetter func() (string, error)
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "write connection event",
+			fields: fields{
+				*bs, fs, nil, nil,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := FileStore{
+				b:                 tt.fields.b,
+				fs:                tt.fields.fs,
+				User:              tt.fields.User,
+				userHomeDirGetter: tt.fields.userHomeDirGetter,
+			}
+			if err := f.WriteConnectionEvent(); (err != nil) != tt.wantErr {
+				t.Errorf("FileStore.WriteConnectionEvent() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			fileContents, err := f.GetFileAsString("/etc/meta/connection_event")
+			if err != nil {
+				t.Errorf("error reading file %s", err)
+			}
+			if fileContents == "" {
+				t.Errorf("file contents are empty")
 			}
 		})
 	}
