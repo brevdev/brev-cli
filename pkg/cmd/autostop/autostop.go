c12be26111c190ccfb2c1ab4dbbcc95ea6d94582
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-12-09 16:05:36 -0800

    add autostop cmd

--- pkg/cmd/autostop/autostop.go
+++ pkg/cmd/autostop/autostop.go
@@ -1,6 +1,9 @@
 package autostop
 
 import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/samber/lo"
+	"github.com/samber/mo"
 	"github.com/spf13/cobra"
 
 	"github.com/brevdev/brev-cli/pkg/entity"
@@ -22,17 +25,14 @@ func NewCmdautostop(t *terminal.Terminal, store autostopStore) *cobra.Command {
 		Long:                  long,
 		Example:               example,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := Runautostop(
-				runAutostopArgs{
-					t:     t,
-					args:  args,
-					store: store,
-				},
-			)
-			if err != nil {
-				return breverrors.WrapAndTrace(err)
-			}
-			return nil
+			return breverrors.WrapAndTrace(
+				Runautostop(
+					runAutostopArgs{
+						t:     t,
+						args:  args,
+						store: store,
+					},
+				))
 		},
 	}
 	return cmd
@@ -40,6 +40,8 @@ func NewCmdautostop(t *terminal.Terminal, store autostopStore) *cobra.Command {
 
 type autostopStore interface {
 	AutoStopWorkspace(workspaceID string) (*entity.Workspace, error)
+	GetActiveOrganizationOrDefault() (*entity.Organization, error)
+	GetWorkspaceByNameOrID(orgID string, nameOrID string) ([]entity.Workspace, error)
 }
 
 type runAutostopArgs struct {
@@ -49,6 +51,28 @@ type runAutostopArgs struct {
 }
 
 func Runautostop(args runAutostopArgs) error {
-	_, err := args.store.AutoStopWorkspace(args.args[0])
-	return breverrors.WrapAndTrace(err)
+	org, err := args.store.GetActiveOrganizationOrDefault()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	envs, err := args.store.GetWorkspaceByNameOrID(org.ID, args.args[0])
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	asResults := lo.Map(
+		envs,
+		func(env entity.Workspace, _ int) mo.Result[*entity.Workspace] {
+			return mo.TupleToResult(args.store.AutoStopWorkspace(env.ID))
+		},
+	)
+	return breverrors.WrapAndTrace(
+		lo.Reduce(
+			asResults,
+			func(acc error, res mo.Result[*entity.Workspace], _ int) error {
+				return multierror.Append(acc, res.Error())
+			},
+			nil,
+		),
+	)
 }
