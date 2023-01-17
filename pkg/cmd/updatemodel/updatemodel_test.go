30e956fe0414df5591083d1b1eb51f2e5ecfd27a
r33drichards <rwendt1337@gmail.com>
2023-01-17 21:50:26 +0000

    test update be

--- pkg/cmd/updatemodel/updatemodel_test.go
+++ pkg/cmd/updatemodel/updatemodel_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/brevdev/brev-cli/pkg/store"
 	"github.com/brevdev/brev-cli/pkg/terminal"
 	"github.com/go-git/go-git/v5"
+	"github.com/spf13/cobra"
 )
 
 type updatemodelStoreMock struct{}
@@ -84,6 +85,54 @@ func Test_updateModel_updateENV(t *testing.T) {
 			}
 			if err := u.updateENV(); (err != nil) != tt.wantErr {
 				t.Errorf("updateModel.updateENV() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_updateModel_RunE(t *testing.T) {
+	type fields struct {
+		t         *terminal.Terminal
+		store     updatemodelStore
+		directory string
+		clone     func(path string, isBare bool, o *git.CloneOptions) (*git.Repository, error)
+		open      func(path string) (repo, error)
+	}
+	type args struct {
+		in0 *cobra.Command
+		in1 []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "test",
+			fields: fields{
+				t:         nil,
+				store:     updatemodelStoreMock{},
+				directory: ".",
+				clone:     mockPlainClone,
+				open:      mockPlainOpen,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := updateModel{
+				t:         tt.fields.t,
+				store:     tt.fields.store,
+				directory: tt.fields.directory,
+				clone:     tt.fields.clone,
+				open:      tt.fields.open,
+			}
+			if err := u.RunE(tt.args.in0, tt.args.in1); (err != nil) != tt.wantErr {
+				t.Errorf("updateModel.RunE() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
