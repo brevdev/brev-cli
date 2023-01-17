e2eaab6bec3361ad9c5b9df9514ce19c602780e7
r33drichards <rwendt1337@gmail.com>
2023-01-17 20:25:08 +0000

    clone new repos from be

--- pkg/cmd/updatemodel/updatemodel.go
+++ pkg/cmd/updatemodel/updatemodel.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 
@@ -34,18 +35,11 @@ func NewCmdupdatemodel(t *terminal.Terminal, store updatemodelStore) *cobra.Comm
 		Short:                 short,
 		Long:                  long,
 		Example:               example,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			err := updateModel{
-				t:         t,
-				args:      args,
-				store:     store,
-				directory: directory,
-			}.Run()
-			if err != nil {
-				return breverrors.WrapAndTrace(err)
-			}
-			return nil
-		},
+		RunE: updateModel{
+			t:         t,
+			store:     store,
+			directory: directory,
+		}.RunE,
 	}
 	cmd.Flags().StringVarP(&directory, "directory", "d", ".", "Directory to run command in")
 	return cmd
@@ -53,12 +47,83 @@ func NewCmdupdatemodel(t *terminal.Terminal, store updatemodelStore) *cobra.Comm
 
 type updateModel struct {
 	t         *terminal.Terminal
-	args      []string
 	store     updatemodelStore
 	directory string
 }
 
-func (u updateModel) Run() error {
+func (u updateModel) RunE(_ *cobra.Command, _ []string) error {
+	// this could be done in one go but this way is easier to reason about
+	err := u.updateBE()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	return nil
+}
+
+func (u updateModel) updateENV() error {
+	remotes, err := u.remotes()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	workspaceID, err := u.store.GetCurrentWorkspaceID()
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	workspace, err := u.store.GetWorkspace(workspaceID)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	reposv1FromBE := workspace.ReposV1
+	reposv1FromENV := lo.Reduce(
+		remotes,
+		func(acc *entity.ReposV1, remote *git.Remote, _ int) *entity.ReposV1 {
+			name := remote.Config().Name
+			url := remote.Config().URLs[0]
+			a := *acc
+			a[entity.RepoName(name)] = entity.RepoV1{
+				Type: entity.GitRepoType,
+				GitRepo: entity.GitRepo{
+					Repository: url,
+				},
+			}
+			return &a
+		},
+		&entity.ReposV1{},
+	)
+
+	envLocalRepoMerger := &repoMerger{
+		acc:   reposv1FromENV,
+		repos: []*entity.ReposV1{reposv1FromBE},
+	}
+
+	errors := lo.Map(
+		envLocalRepoMerger.ReposToClone(),
+		func(repo *entity.RepoV1, _ int) error {
+			return nil
+		},
+	)
+
+	return breverrors.WrapAndTrace(
+		lo.Reduce(
+			errors,
+			func(acc error, err error, _ int) error {
+				if acc != nil && err != nil {
+					return multierror.Append(acc, err)
+				}
+				if acc == nil && err != nil {
+					return err
+				}
+				return acc
+			},
+			nil,
+		),
+	)
+}
+
+func (u updateModel) updateBE() error {
 	remotes, err := u.remotes()
 	if err != nil {
 		return breverrors.WrapAndTrace(err)
@@ -99,7 +164,7 @@ func (u updateModel) Run() error {
 	_, err = u.store.ModifyWorkspace(
 		workspaceID,
 		&store.ModifyWorkspaceRequest{
-			ReposV1: beLocalRepoMerger.Merge(),
+			ReposV1: beLocalRepoMerger.MergeBE(),
 		},
 	)
 	if err != nil {
@@ -113,7 +178,7 @@ type repoMerger struct {
 	repos []*entity.ReposV1
 }
 
-func (r *repoMerger) Merge() *entity.ReposV1 {
+func (r *repoMerger) MergeBE() *entity.ReposV1 {
 	for _, repo := range r.repos {
 		for k, v := range *repo {
 			if _, ok := (*r.acc)[k]; ok {
@@ -132,6 +197,32 @@ func (r *repoMerger) Merge() *entity.ReposV1 {
 		}
 	}
 	return r.acc
+}
+
+func (r *repoMerger) ReposToClone() []*entity.RepoV1 {
+	// repos present in the BE but not in the ENV
+	return lo.Filter(
+		r.accValues(),
+		func(repo *entity.RepoV1, _ int) bool {
+			_, valueInENV := lo.Find(
+				r.reposValues(),
+				func(repo *entity.RepoV1) bool {
+					return repo.GitRepo.Repository == repo.GitRepo.Repository
+				},
+			)
+			return !valueInENV
+		},
+	)
+}
+
+func (r repoMerger) reposValues() []*entity.RepoV1 {
+	values := []*entity.RepoV1{}
+	for _, repo := range r.repos {
+		for _, v := range *repo {
+			values = append(values, &v)
+		}
+	}
+	return values
 }
 
 func (r repoMerger) accValues() []*entity.RepoV1 {
