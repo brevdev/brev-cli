e9abc39b001df96939fa2217cfa8e6139e03fe55
Travis Cline <travis.cline@gmail.com>
2024-04-29 10:47:49 -0400

    ollama: Add start of ollama subcommand

---
+++ pkg/cmd/ollama/ollama.go
@@ -0,0 +1,223 @@
+package ollama
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/brevdev/brev-cli/pkg/analytics"
+	"github.com/brevdev/brev-cli/pkg/cmd/cmderrors"
+	"github.com/brevdev/brev-cli/pkg/cmd/completions"
+	"github.com/brevdev/brev-cli/pkg/cmd/refresh"
+	"github.com/brevdev/brev-cli/pkg/cmd/util"
+	"github.com/brevdev/brev-cli/pkg/entity"
+	breverrors "github.com/brevdev/brev-cli/pkg/errors"
+	"github.com/brevdev/brev-cli/pkg/store"
+	"github.com/brevdev/brev-cli/pkg/terminal"
+	uutil "github.com/brevdev/brev-cli/pkg/util"
+	"github.com/briandowns/spinner"
+	"github.com/hashicorp/go-multierror"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	ollamaLong    = "[command in beta] This will "
+	ollamaExample = "brev ollama ..."
+)
+
+type OllamaStore interface {
+	util.GetWorkspaceByNameOrIDErrStore
+	refresh.RefreshStore
+	UpdateUser(string, *entity.UpdateUser) (*entity.User, error)
+	GetOrganizations(options *store.GetOrganizationsOptions) ([]entity.Organization, error)
+	GetWorkspaces(organizationID string, options *store.GetWorkspacesOptions) ([]entity.Workspace, error)
+	StartWorkspace(workspaceID string) (*entity.Workspace, error)
+	GetActiveOrganizationOrDefault() (*entity.Organization, error)
+	GetWorkspace(workspaceID string) (*entity.Workspace, error)
+	GetWindowsDir() (string, error)
+	IsWorkspace() (bool, error)
+}
+
+func NewCmdOllama(t *terminal.Terminal, store OllamaStore, noLoginStartStore OllamaStore) *cobra.Command {
+	var waitForSetupToFinish bool
+	var directory string
+	var host bool
+
+	cmd := &cobra.Command{
+		Annotations:           map[string]string{"ssh": ""},
+		Use:                   "ollama",
+		DisableFlagsInUseLine: true,
+		Short:                 "[beta] ollama",
+		Long:                  ollamaLong,
+		Example:               ollamaExample,
+		Args:                  cmderrors.TransformToValidationError(cobra.ExactArgs(1)),
+		ValidArgsFunction:     completions.GetAllWorkspaceNameCompletionHandler(noLoginStartStore, t),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			setupDoneString := "------ Git repo cloned ------"
+			if waitForSetupToFinish {
+				setupDoneString = "------ Done running execs ------"
+			}
+			err := runOllamaCommand(t, store, args[0], setupDoneString, directory, host)
+			if err != nil {
+				return breverrors.WrapAndTrace(err)
+			}
+			return nil
+		},
+	}
+	// cmd.Flags().BoolVarP(&host, "host", "", false, "ssh into the host machine instead of the container")
+	// cmd.Flags().BoolVarP(&waitForSetupToFinish, "wait", "w", false, "wait for setup to finish")
+	// cmd.Flags().StringVarP(&directory, "dir", "d", "", "directory to ollama")
+
+	return cmd
+}
+
+// Fetch workspace info, then ollama code editor
+func runOllamaCommand(t *terminal.Terminal, tstore OllamaStore, wsIDOrName string, setupDoneString string, directory string, host bool) error { //nolint:funlen // define brev command
+	// todo check if workspace is stopped and start if it if it is stopped
+	fmt.Println("finding your instance...")
+	res := refresh.RunRefreshAsync(tstore)
+	workspace, err := util.GetUserWorkspaceByNameOrIDErr(tstore, wsIDOrName)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	_ = res
+	if workspace.Status == "STOPPED" { // we start the env for the user
+		err = startWorkspaceIfStopped(t, tstore, wsIDOrName, workspace)
+		if err != nil {
+			return breverrors.WrapAndTrace(err)
+		}
+	}
+	err = pollUntil(t, workspace.ID, "RUNNING", tstore)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	workspace, err = util.GetUserWorkspaceByNameOrIDErr(tstore, wsIDOrName)
+
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	if workspace.Status != "RUNNING" {
+		return breverrors.WorkspaceNotRunning{Status: workspace.Status}
+	}
+
+	// Call analytics for ollama
+	_ = pushOllamaAnalytics(tstore, workspace)
+	return nil
+}
+
+func pushOllamaAnalytics(tstore OllamaStore, workspace *entity.Workspace) error {
+	userID := ""
+	user, err := tstore.GetCurrentUser()
+	if err != nil {
+		userID = workspace.CreatedByUserID
+	} else {
+		userID = user.ID
+	}
+	data := analytics.EventData{
+		EventName: "Brev Ollama",
+		UserID:    userID,
+		Properties: map[string]string{
+			"instanceId": workspace.ID,
+		},
+	}
+	err = analytics.TrackEvent(data)
+	return breverrors.WrapAndTrace(err)
+}
+
+func pollUntil(t *terminal.Terminal, wsid string, state string, ollamaStore OllamaStore) error {
+	s := t.NewSpinner()
+	isReady := false
+	s.Suffix = " hang tight 🤙"
+	s.Start()
+	for !isReady {
+		time.Sleep(5 * time.Second)
+		ws, err := ollamaStore.GetWorkspace(wsid)
+		if err != nil {
+			return breverrors.WrapAndTrace(err)
+		}
+		s.Suffix = "  workspace is currently " + strings.ToLower(ws.Status)
+		if ws.Status == state {
+			s.Suffix = "Workspace is ready!"
+			s.Stop()
+			isReady = true
+		}
+	}
+	return nil
+}
+
+func startWorkspaceIfStopped(t *terminal.Terminal, tstore OllamaStore, wsIDOrName string, workspace *entity.Workspace) error {
+	startedWorkspace, err := tstore.StartWorkspace(workspace.ID)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+	t.Vprintf(t.Yellow("Instance %s is starting. \n\n", startedWorkspace.Name))
+	workspace, err = util.GetUserWorkspaceByNameOrIDErr(tstore, wsIDOrName)
+	if err != nil {
+		return breverrors.WrapAndTrace(err)
+	}
+
+	return nil
+}
+
+func tryToInstallExtensions(
+	t *terminal.Terminal,
+	extIDs []string,
+) {
+	for _, extID := range extIDs {
+		extInstalled, err0 := uutil.IsVSCodeExtensionInstalled(extID)
+		if !extInstalled {
+			err1 := uutil.InstallVscodeExtension(extID)
+			isRemoteInstalled, err2 := uutil.IsVSCodeExtensionInstalled(extID)
+			if !isRemoteInstalled {
+				err := multierror.Append(err0, err1, err2)
+				t.Print(t.Red("Couldn't install the necessary VSCode extension automatically.\nError: " + err.Error()))
+				t.Print("\tPlease install VSCode and the following VSCode extension: " + t.Yellow(extID) + ".\n")
+				_ = terminal.PromptGetInput(terminal.PromptContent{
+					Label:      "Hit enter when finished:",
+					ErrorMsg:   "error",
+					AllowEmpty: true,
+				})
+			}
+		}
+	}
+}
+
+func waitForSSHToBeAvailable(t *terminal.Terminal, s *spinner.Spinner, sshAlias string) error {
+	counter := 0
+	for {
+		cmd := exec.Command("ssh", "-o", "ConnectTimeout=1", sshAlias, "echo", " ")
+		out, err := cmd.CombinedOutput()
+		if err == nil {
+			return nil
+		}
+
+		outputStr := string(out)
+		stdErr := strings.Split(outputStr, "\n")[1]
+
+		if counter == 160 || !store.SatisfactorySSHErrMessage(stdErr) {
+			return breverrors.WrapAndTrace(errors.New("\n" + stdErr))
+		}
+
+		if counter == 10 {
+			s.Suffix = t.Green(" waiting for SSH connection to be available 🤙")
+		}
+
+		counter++
+		time.Sleep(1 * time.Second)
+	}
+}
+
+type vscodePathStore interface {
+	GetWindowsDir() (string, error)
+}
+
+func getWindowsVsCodePaths(store vscodePathStore) []string {
+	wd, _ := store.GetWindowsDir()
+	paths := append([]string{}, fmt.Sprintf("%s/AppData/Local/Programs/Microsoft VS Code/Code.exe", wd), fmt.Sprintf("%s/AppData/Local/Programs/Microsoft VS Code/bin/code", wd))
+	return paths
+}
