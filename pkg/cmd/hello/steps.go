bcd8d8a530ef5aba78adfb6ec21b3d2f75ad22d6
hakhandelwal11 <hakhandelwal@nvidia.com>
2026-04-07 20:06:17 +0530

    fix: add unit test

--- pkg/cmd/hello/steps.go
+++ pkg/cmd/hello/steps.go
@@ -242,7 +242,7 @@ func doBrevShellOnboarding(
 		if err1 != nil {
 			return breverrors.WrapAndTrace(err1)
 		}
-		if res.HasRunBrevShell {
+		if shellOnboardingPollDone(res) {
 			spinner.Suffix = spinnerSuffix
 			time.Sleep(250 * time.Millisecond)
 			spinner.Stop()
@@ -298,7 +298,7 @@ func doVsCodeOnboarding(
 		if err1 != nil {
 			return breverrors.WrapAndTrace(err1)
 		}
-		if shellOnboardingPollDone(res) {
+		if res.HasRunBrevOpen {
 			spinner.Suffix = spinnerSuffix
 			time.Sleep(250 * time.Millisecond)
 			spinner.Stop()
