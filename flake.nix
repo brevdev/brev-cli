a79dfd8be8bd0f49d00fd228bb2d70872d446e74
chandler-barlow <cbarlow@nvidia.com>
2026-04-08 23:50:14 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -1,20 +1,19 @@
 {
-  description = "golang instance ";
-  
-inputs = {
+  description = "golang instance";
+
+  inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-     flake-utils.lib.eachDefaultSystem
-       (system:
-         let pkgs = nixpkgs.legacyPackages.${system}; in
-         {
-          devShell = import ./shell.nix { inherit pkgs system; };
-          shellHook = ''
-            echo "hallo"
-          '';
-         }
-       );
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
