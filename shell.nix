fd9e23e7e0da0b1653cf9263f54e2d5a6b16b272
chandler-barlow <cbarlow@nvidia.com>
2026-04-14 10:31:25 -0700

    updated shell

--- shell.nix
+++ shell.nix
@@ -1,43 +1,16 @@
-{
-  system ? builtins.currentSystem,
-  pkgs ? import <nixpkgs> { inherit system; },
-}:
+{ pkgs }:
 
-with pkgs;
-let
-  pkgs =
-    import
-      (builtins.fetchGit {
-        # Descriptive name to make the store path easier to identify
-        name = "my-old-revision";
-        url = "https://github.com/NixOS/nixpkgs/";
-        ref = "refs/heads/nixpkgs-unstable";
-        rev = "ff8b619cfecb98bb94ae49ca7ceca937923a75fa";
-        #  }) {};
-      })
-      {
-        inherit system;
-      };
-  flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  olderVersionOfGolangci-lint = pkgs.golangci-lint;
-  #  system = builtins.currentSystem;
-in
-mkShell {
-  nativeBuildInputs = [
-    go_1_19
+pkgs.mkShell {
+  packages = with pkgs; [
+    go
     gopls
     tmux
     gofumpt
-    olderVersionOfGolangci-lint
+    golangci-lint
     gosec
     delve
-    go-tools
+    gotools
     gotests
     gomodifytags
   ];
-  # pkgs.system="x86_64-linux";
 }
-# )
